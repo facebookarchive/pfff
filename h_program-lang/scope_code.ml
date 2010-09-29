df749a9c4bfd9f43c03a1ff079ded09ff5b31f15
pad <pad@facebook.com>
2010-09-29 10:49:51 -0700

    factorize scope_php code in h_program-lang/scope_code.ml

---
+++ h_program-lang/scope_code.ml
@@ -0,0 +1,100 @@
+(* Yoann Padioleau
+ * 
+ * Copyright (C) 2009-2010 Facebook
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
+
+open Common
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
+(* 
+ * It would be more convenient to move this file elsewhere like in analyse_xxx/
+ * but we want our AST to contain scope annotations so it's convenient to 
+ * have the type definition of scope there.
+ *)
+
+(*****************************************************************************)
+(* Types *)
+(*****************************************************************************)
+
+(* todo? could use polymorphic variant for that ? the scoping will
+ * be differerent for each language but they will also have stuff
+ * in common which may be a good spot for polymorphic variant.
+ *)
+type scope = 
+  | Global
+  | Local 
+  | Param
+
+  | Class
+
+  | LocalExn
+  | LocalIterator
+
+  (* php specific? *)
+  | ListBinded
+  (* | Class ? *)
+
+  | NoScope
+
+(*****************************************************************************)
+(* String-of *)
+(*****************************************************************************)
+
+let string_of_scope = function
+  | Global ->  "Global"
+  | Local ->  "Local"
+  | Param ->  "Param"
+  | Class ->  "Class"
+  | LocalExn ->  "LocalExn"
+  | LocalIterator ->  "LocalIterator"
+  | ListBinded ->  "ListBinded"
+  | NoScope ->  "NoScope"
+
+(*****************************************************************************)
+(* Meta *)
+(*****************************************************************************)
+
+let vof_scope x = 
+  match x with
+  | Global -> Ocaml.VSum (("Global", []))
+  | Local -> Ocaml.VSum (("Local", []))
+  | Param -> Ocaml.VSum (("Param", []))
+  | Class -> Ocaml.VSum (("Class", []))
+  | LocalExn -> Ocaml.VSum (("LocalExn", []))
+  | LocalIterator -> Ocaml.VSum (("LocalIterator", []))
+  | ListBinded -> Ocaml.VSum (("ListBinded", []))
+  | NoScope -> Ocaml.VSum (("NoScope", []))
+
+let sexp_of_scope x =
+  match x with
+  | Global -> Sexp.Atom "Global"
+  | Local -> Sexp.Atom "Local"
+  | Param -> Sexp.Atom "Param"
+  | NoScope -> Sexp.Atom "NoScope"
+  | ListBinded -> Sexp.Atom "ListBinded"
+  | LocalIterator -> Sexp.Atom "LocalIterator"
+  | LocalExn -> Sexp.Atom "LocalExn"
+  | Class -> Sexp.Atom "Class"
+
+let map_scope =
+  function
+  | Global -> Global
+  | Local -> Local
+  | Param -> Param
+  | NoScope -> NoScope
+  | ListBinded -> ListBinded
+  | LocalIterator -> LocalIterator
+  | LocalExn -> LocalExn
+  | Class -> Class
