cbcb7efcaf5d2ef113d8097d3bb954f9b61298eb
pad <pad@facebook.com>
2010-10-07 17:34:01 -0700

    visitor_ml: auto generated for expr

--- lang_ml/parsing/visitor_ml.ml
+++ lang_ml/parsing/visitor_ml.ml
@@ -67,6 +67,16 @@ and v_wrap _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
 and v_wrap1 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
 and v_wrap2 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
 
+and v_wrap11 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap12 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap13 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap14 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap15 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap16 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap17 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+and v_wrap18 _of_a (v1, v2) = let v1 = _of_a v1 and v2 = v_info v2 in ()
+
+
 and v_paren _of_a (v1, v2, v3) =
   let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
 and v_paren1 _of_a (v1, v2, v3) =
@@ -74,9 +84,17 @@ and v_paren1 _of_a (v1, v2, v3) =
 and v_paren2 _of_a (v1, v2, v3) =
   let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
 
+and v_paren11 _of_a (v1, v2, v3) =
+  let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
+and v_paren12 _of_a (v1, v2, v3) =
+  let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
+
 and v_brace _of_a (v1, v2, v3) =
   let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
 and v_brace1 _of_a (v1, v2, v3) =
+  let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
+
+and v_brace11 _of_a (v1, v2, v3) =
   let v1 = v_tok v1 and v2 = _of_a v2 and v3 = v_tok v3 in ()
 
 
@@ -86,16 +104,27 @@ and v_comma_list _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_comma_list1 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_comma_list2 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 
+and v_comma_list11 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+
 and v_and_list _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_and_list1 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_and_list2 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 
+and v_and_list13 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+
 and v_pipe_list _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_pipe_list1 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+
+and v_pipe_list11 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+and v_pipe_list12 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+and v_pipe_list13 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 
 and v_semicolon_list _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_semicolon_list1 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_semicolon_list2 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+
+and v_semicolon_list11 _of_a = v_list (Ocaml.v_either _of_a v_tok)
+and v_semicolon_list12 _of_a = v_list (Ocaml.v_either _of_a v_tok)
 
 and v_star_list _of_a = v_list (Ocaml.v_either _of_a v_tok)
 and v_star_list1 _of_a = v_list (Ocaml.v_either _of_a v_tok)
@@ -170,15 +199,144 @@ and v_ty_params =
   | TyParamMulti v1 ->
       let v1 = v_paren1 (v_comma_list1 v_ty_parameter) v1 in ()
 and v_ty_parameter (v1, v2) = let v1 = v_tok v1 and v2 = v_name v2 in ()
-and v_expr v = 
- 
- match v with
+
+
+and v_expr =
+  function
+  | C v1 -> let v1 = v_constant v1 in ()
+  | L v1 -> let v1 = v_long_name v1 in ()
+  | Cons ((v1, v2)) ->
+      let v1 = v_long_name v1 and v2 = v_option v_expr v2 in ()
+  | Tuple v1 -> let v1 = v_comma_list11 v_expr v1 in ()
+  | ParenExpr v1 -> let v1 = v_paren12 v_expr v1 in ()
+  | Sequence v1 -> let v1 = v_paren11 v_seq_expr v1 in ()
+  | Prefix ((v1, v2)) ->
+      let v1 = v_wrap18 v_string v1 and v2 = v_expr v2 in ()
+  | Infix ((v1, v2, v3)) ->
+      let v1 = v_expr v1
+      and v2 = v_wrap17 v_string v2
+      and v3 = v_expr v3
+      in ()
+  | FunCallSimple ((v1, v2)) ->
+      let v1 = v_long_name v1 and v2 = v_list v_argument v2 in ()
+  | FunCall ((v1, v2)) ->
+      let v1 = v_expr v1 and v2 = v_list v_argument v2 in ()
+  | RefAccess ((v1, v2)) -> let v1 = v_tok v1 and v2 = v_expr v2 in ()
+  | RefAssign ((v1, v2, v3)) ->
+      let v1 = v_expr v1 and v2 = v_tok v2 and v3 = v_expr v3 in ()
+  | FieldAccess ((v1, v2, v3)) ->
+      let v1 = v_expr v1 and v2 = v_tok v2 and v3 = v_long_name v3 in ()
+  | FieldAssign ((v1, v2, v3, v4, v5)) ->
+      let v1 = v_expr v1
+      and v2 = v_tok v2
+      and v3 = v_long_name v3
+      and v4 = v_tok v4
+      and v5 = v_expr v5
+      in ()
+  | Record v1 -> let v1 = v_brace11 v_record_expr v1 in ()
+  | ObjAccess ((v1, v2, v3)) ->
+      let v1 = v_expr v1 and v2 = v_tok v2 and v3 = v_name v3 in ()
+  | New ((v1, v2)) -> let v1 = v_tok v1 and v2 = v_long_name v2 in ()
+  | LetIn ((v1, v2, v3, v4, v5)) ->
+      let v1 = v_tok v1
+      and v2 = v_rec_opt v2
+      and v3 = v_and_list13 v_let_binding v3
+      and v4 = v_tok v4
+      and v5 = v_seq_expr v5
+      in ()
+  | Fun ((v1, v2, v3)) ->
+      let v1 = v_tok v1
+      and v2 = v_list v_parameter v2
+      and v3 = v_match_action v3
+      in ()
+  | Function ((v1, v2)) ->
+      let v1 = v_tok v1 and v2 = v_pipe_list13 v_match_case v2 in ()
+  | If ((v1, v2, v3, v4, v5)) ->
+      let v1 = v_tok v1
+      and v2 = v_seq_expr v2
+      and v3 = v_tok v3
+      and v4 = v_expr v4
+      and v5 =
+        v_option (fun (v1, v2) -> let v1 = v_tok v1 and v2 = v_expr v2 in ())
+          v5
+      in ()
+  | Match ((v1, v2, v3, v4)) ->
+      let v1 = v_tok v1
+      and v2 = v_seq_expr v2
+      and v3 = v_tok v3
+      and v4 = v_pipe_list12 v_match_case v4
+      in ()
+  | Try ((v1, v2, v3, v4)) ->
+      let v1 = v_tok v1
+      and v2 = v_seq_expr v2
+      and v3 = v_tok v3
+      and v4 = v_pipe_list11 v_match_case v4
+      in ()
+  | While ((v1, v2, v3, v4, v5)) ->
+      let v1 = v_tok v1
+      and v2 = v_seq_expr v2
+      and v3 = v_tok v3
+      and v4 = v_seq_expr v4
+      and v5 = v_tok v5
+      in ()
+  | For ((v1, v2, v3, v4, v5, v6, v7, v8, v9)) ->
+      let v1 = v_tok v1
+      and v2 = v_name v2
+      and v3 = v_tok v3
+      and v4 = v_seq_expr v4
+      and v5 = v_for_direction v5
+      and v6 = v_seq_expr v6
+      and v7 = v_tok v7
+      and v8 = v_seq_expr v8
+      and v9 = v_tok v9
+      in ()
   | ExprTodo -> ()
+
+and v_constant =
+  function
+  | Int v1 -> let v1 = v_wrap16 v_string v1 in ()
+  | Float v1 -> let v1 = v_wrap15 v_string v1 in ()
+  | Char v1 -> let v1 = v_wrap14 v_string v1 in ()
+  | String v1 -> let v1 = v_wrap13 v_string v1 in ()
+
+and v_record_expr =
+  function
+  | RecordNormal v1 -> let v1 = v_semicolon_list12 v_field_and_expr v1 in ()
+  | RecordWith ((v1, v2, v3)) ->
+      let v1 = v_expr v1
+      and v2 = v_tok v2
+      and v3 = v_semicolon_list11 v_field_and_expr v3
+      in ()
+and v_field_and_expr =
+  function
+  | FieldExpr ((v1, v2, v3)) ->
+      let v1 = v_long_name v1 and v2 = v_tok v2 and v3 = v_expr v3 in ()
+  | FieldImplicitExpr v1 -> let v1 = v_long_name v1 in ()
+
+and v_argument v = v_unit v
+
+and v_match_action =
+  function
+  | Action ((v1, v2)) -> let v1 = v_tok v1 and v2 = v_seq_expr v2 in ()
+  | WhenAction ((v1, v2, v3, v4)) ->
+      let v1 = v_tok v1
+      and v2 = v_seq_expr v2
+      and v3 = v_tok v3
+      and v4 = v_seq_expr v4
+      in ()
+and v_match_case (v1, v2) =
+  let v1 = v_pattern v1 and v2 = v_match_action v2 in ()
+and v_for_direction =
+  function
+  | To v1 -> let v1 = v_tok v1 in ()
+  | Downto v1 -> let v1 = v_tok v1 in ()
 
 and v_seq_expr v = v_semicolon_list1 v_expr v
 and v_pattern v = v_unit v
 and v_simple_pattern v = v_unit v
 and v_labeled_simple_pattern v = v_unit v
+and v_parameter v = v_labeled_simple_pattern v
+
 and v_let_binding =
   function
   | LetClassic v1 -> let v1 = v_let_def v1 in ()
