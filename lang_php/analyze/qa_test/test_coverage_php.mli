b598d07b798a4c2490180f514a0467f4ae7bce86
pad <pad@facebook.com>
2010-10-09 12:46:50 -0700

    test_coverage_php: renaming type

--- lang_php/analyze/qa_test/test_coverage_php.mli
+++ lang_php/analyze/qa_test/test_coverage_php.mli
@@ -4,8 +4,8 @@
 type tests_coverage = (Common.filename (* source *), tests_score) Common.assoc
  and tests_score = (Common.filename (* a test *) * float) list
 
-type files_coverage = (Common.filename, file_coverage) Common.assoc
- and file_coverage = {
+type lines_coverage = (Common.filename, file_lines_coverage) Common.assoc
+ and file_lines_coverage = {
    covered_call_sites: int list;
    call_sites: int list;
  }
@@ -38,14 +38,14 @@ val coverage_tests :
   (Common.filename * string (* error *)) list (* tests with pbs *)
 
 
-val files_coverage_from_tests:  
+val lines_coverage_from_tests:  
   ?skip_call:(Xdebug.call_trace -> bool) ->
   ?is_directive_to_filter:(string -> bool) ->
   php_cmd_run_test:(php_interpreter:string -> Common.filename -> string) ->
   all_test_files:Common.filename list ->
   all_files:Common.filename list ->
   unit ->
-  files_coverage
+  lines_coverage
 
 
 (* for percentage statistics per file *)
@@ -64,14 +64,14 @@ val unittest: OUnit.test
 
 (* input/output *)
 val json_of_tests_coverage: tests_coverage -> Json_type.json_type
-val json_of_files_coverage: files_coverage -> Json_type.json_type
+val json_of_lines_coverage: lines_coverage -> Json_type.json_type
 
 val tests_coverage_of_json: Json_type.json_type -> tests_coverage
-val files_coverage_of_json: Json_type.json_type -> files_coverage
+val lines_coverage_of_json: Json_type.json_type -> lines_coverage
 
 (* shortcuts *)
 val save_tests_coverage: tests_coverage -> Common.filename -> unit
 val load_tests_coverage: Common.filename -> tests_coverage
 
-val save_files_coverage: files_coverage -> Common.filename -> unit
-val load_files_coverage: Common.filename -> files_coverage
+val save_lines_coverage: lines_coverage -> Common.filename -> unit
+val load_lines_coverage: Common.filename -> lines_coverage
