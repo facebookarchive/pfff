dd50723c307fd1ba5868fd4004d2d88feaa423e5
pad <pad@facebook.com>
2010-10-21 10:34:19 -0700

    lang_lisp: ./pfff -tokens_lisp tests/lisp/foo.lisp now works

--- lang_lisp/parsing/flag_parsing_lisp.ml
+++ lang_lisp/parsing/flag_parsing_lisp.ml
@@ -2,3 +2,5 @@
 
 let verbose_lexing = ref false
 let verbose_parsing = ref false
+
+let debug_lexer = ref false
--- lang_lisp/parsing/parse_lisp.ml
+++ lang_lisp/parsing/parse_lisp.ml
@@ -19,6 +19,10 @@ module Flag = Flag_parsing_lisp
 
 module PI = Parse_info
 
+(* we don't need a full grammar for lisp code, so we put everything,
+ * the token type, the helper in parser_ml. No token_helpers_lisp.ml
+ *)
+module TH = Parser_lisp
 
 (*****************************************************************************)
 (* Prelude *)
@@ -51,10 +55,47 @@ let lexbuf_to_strpos lexbuf     =
 (* Lexing only *)
 (*****************************************************************************)
 
+(* could factorize and take the tokenf and visitor_of_infof in argument
+ * but sometimes copy-paste is ok.
+ *)
 let tokens2 file = 
-  let table     = PI.full_charpos_to_pos_large file in
+  let table     = Parse_info.full_charpos_to_pos_large file in
 
-  raise Todo
+  Common.with_open_infile file (fun chan -> 
+    let lexbuf = Lexing.from_channel chan in
+
+    try 
+      let ftoken lexbuf = 
+        Lexer_lisp.token lexbuf
+      in
+      
+      let rec tokens_aux acc = 
+        let tok = ftoken lexbuf in
+        if !Flag.debug_lexer then Common.pr2_gen tok;
+
+        let tok = tok +> TH.visitor_info_of_tok (fun ii -> 
+        { ii with PI.token=
+          (* could assert pinfo.filename = file ? *)
+           match PI.pinfo_of_info ii with
+           | PI.OriginTok pi ->
+               PI.OriginTok 
+                 (PI.complete_parse_info_large file table pi)
+           | _ -> raise Todo
+        })
+        in
+        
+        if TH.is_eof tok
+        then List.rev (tok::acc)
+        else tokens_aux (tok::acc)
+      in
+      tokens_aux []
+  with
+  | Lexer_lisp.Lexical s -> 
+      failwith ("lexical error " ^ s ^ "\n =" ^ 
+                 (PI.error_message file (lexbuf_to_strpos lexbuf)))
+  | e -> raise e
+ )
+
 
 let tokens a = 
   Common.profile_code "Parse_lisp.tokens" (fun () -> tokens2 a)
