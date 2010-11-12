a2b041d3d3d82603abef866d27fcac1817b0bd22
pad <pad@facebook.com>
2010-11-12 09:33:31 -0800

    add doc on xhprof metrics

---
+++ lang_php/analyze/tools/xhprof.ml
@@ -0,0 +1,49 @@
+
+(*
+ *
+ * $ phpsh
+ * 
+ * Starting php
+ * Loaded fb codebase.
+ * type 'h' or 'help' to see instructions & features
+ * php> xhprof_get_possible_metrics()
+ * 
+ * php> =xhprof_get_possible_metrics()
+ * array(
+ *   "wt" => array(
+ *     0 => "Wall",
+ *     1 => "microsecs",
+ *     2 => "walltime",
+ *   ),
+ *   "ut" => array(
+ *     0 => "User",
+ *     1 => "microsecs",
+ *     2 => "user cpu time",
+ *   ),
+ *   "st" => array(
+ *     0 => "Sys",
+ *     1 => "microsecs",
+ *     2 => "system cpu time",
+ *   ),
+ *   "cpu" => array(
+ *     0 => "Cpu",
+ *     1 => "microsecs",
+ *     2 => "cpu time",
+ *   ),
+ *   "mu" => array(
+ *     0 => "MUse",
+ *     1 => "bytes",
+ *     2 => "memory usage",
+ *   ),
+ *   "pmu" => array(
+ *     0 => "PMUse",
+ *     1 => "bytes",
+ *     2 => "peak memory usage",
+ *   ),
+ *   "samples" => array(
+ *     0 => "Samples",
+ *     1 => "samples",
+ *     2 => "cpu time",
+ *   ),
+ * )
+ *)
