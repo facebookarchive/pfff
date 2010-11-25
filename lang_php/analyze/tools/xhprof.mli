b81d197c8d02efff703947c6eb9ea1b827ac7b15
pad <pad@facebook.com>
2010-11-25 11:37:04 -0800

    added xhprof.ml (migrated from facebook/.../)

---
+++ lang_php/analyze/tools/xhprof.mli
@@ -0,0 +1,17 @@
+
+type xhprof_call = {
+  src: xhprof_entity;
+  dest: xhprof_entity;
+}
+ and xhprof_entity =
+  | Function of string
+  | Method of string (* class *) * string (* method *)
+  | Main
+  | PruneChild
+  | RunInit of Common.filename
+  | Misc of string
+
+val string_of_xhprof_entity: xhprof_entity -> string
+val parse_xhprof_entity_string: string -> xhprof_entity
+
+val parse_caller_callee_string: string -> xhprof_call
