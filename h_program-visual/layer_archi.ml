99711c347a4851585c3d6a454b475f5df5955180
pad <pad@facebook.com>
2010-12-11 18:31:19 -0800

    added skeleton for layer_archi.ml

---
+++ h_program-visual/layer_archi.ml
@@ -0,0 +1,35 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2010 Facebook
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
+
+open Common 
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
+(*****************************************************************************)
+(* Main entry point *)
+(*****************************************************************************)
+let gen_layer dir ~output =
+  raise Todo
+
+(*****************************************************************************)
+(* Actions *)
+(*****************************************************************************)
+
+let actions () = [
+  "-gen_archi_layer", " <dir> <layerfile>",
+  Common.mk_action_2_arg (fun dir output -> gen_layer dir ~output);
+]
---
+++ h_program-visual/layer_archi.mli
@@ -0,0 +1,5 @@
+
+val gen_layer: 
+  Common.dirname -> output:Common.filename -> unit
+
+val actions : unit -> Common.cmdline_actions
