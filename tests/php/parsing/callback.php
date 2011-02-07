224c9669c872f33902046f5c8ff81869bb09b11c
pad <pad@fb.com>
2011-02-07 15:17:16 -0800

    callback.php: more cases

--- tests/php/parsing/callback.php
+++ tests/php/parsing/callback.php
@@ -5,3 +5,14 @@ foo('A::callback2', 1);
 foo(array('A', 'callback3'), 1);
 foo(array($this, 'callback4'), 1);
 foo(array($object, 'callback5'), 1);
+
+class X {
+  public function foo() {
+    foo(array(__CLASS__, 'callback6'), 1);
+  }
+}
+
+function foo() {
+  map(__FUNCTION__, array(1,2));
+  map(__function__, array(1,2));
+}
