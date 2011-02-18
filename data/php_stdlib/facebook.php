1d2fe19797f6774026dd41c7d6942d7c435fc71d
pad <pad@fb.com>
2011-02-17 16:51:29 -0800

    lang_php: import the constants from HPHP.

--- data/php_stdlib/facebook.php
+++ data/php_stdlib/facebook.php
@@ -1,8 +1,10 @@
 <?php
 
-// see also static $builtin_defines =
+
+// see also static $builtin_defines = ...
 // in www/flib/_bin/_checkModuleLib.php
-define('CLOCK_THREAD_CPUTIME_ID', 1);
+// now in hphp_constants.php
+// define('CLOCK_THREAD_CPUTIME_ID', 1);
 
 // flib/ code is supposed to never use flib_init() so never use
 // any FLIB_CONTEXT_xxx constants, but some of our
@@ -10,7 +12,7 @@ define('CLOCK_THREAD_CPUTIME_ID', 1);
 //   if (ModuleStack::getContext() === FLIB_CONTEXT_TEST) ...
 define('FLIB_CONTEXT_TEST', 1);
 
-// fib/init/http/safety.php also uses those constants to determine which 
+// flib/init/http/safety.php also uses those constants to determine which 
 // pages should be 404'ed in the secure tier.
 // mostly copy paste of flib/__flib.php
 define('FLIB_CONTEXT_NOOP', -1);
@@ -33,22 +35,22 @@ define('FLIB_CONTEXT_DEVSITE', 15);
 define('FLIB_CONTEXT_SECURE_WEB', 16);
 define('FLIB_CONTEXT_CANVAS', 17);
 define('FLIB_CONTEXT_CODESITE', 18);
-
+define('FLIB_CONTEXT_FBIPHONE_ENDPOINT', 19);
 
 define('T_XHP_TEXT', 1);
 
 // see http://www.intern.facebook.com/intern/hphp/doc/index.php?file=threading
-define('GLOBAL_STATE_IGNORE', 1);
-define('GLOBAL_STATE_OVERWRITE', 1);
-define('GLOBAL_STATE_SKIP', 1);
-define('GLOBAL_SYMBOL_GLOBAL_VARIABLE', 1);
-define('GLOBAL_SYMBOL_STATIC_VARIABLE', 1);
-define('GLOBAL_SYMBOL_CLASS_STATIC', 1);
-define('GLOBAL_SYMBOL_DYNAMIC_CONSTANT', 1);
-define('GLOBAL_SYMBOL_FILE_INCLUDE', 1);
-define('GLOBAL_SYMBOL_REDECLARED_FUNCTION', 1);
-define('GLOBAL_SYMBOL_REDECLARED_CLASS', 1);
-
+// now in hphp_constants.php
+//define('GLOBAL_STATE_IGNORE', 1);
+//define('GLOBAL_STATE_OVERWRITE', 1);
+//define('GLOBAL_STATE_SKIP', 1);
+//define('GLOBAL_SYMBOL_GLOBAL_VARIABLE', 1);
+//define('GLOBAL_SYMBOL_STATIC_VARIABLE', 1);
+//define('GLOBAL_SYMBOL_CLASS_STATIC', 1);
+//define('GLOBAL_SYMBOL_DYNAMIC_CONSTANT', 1);
+//define('GLOBAL_SYMBOL_FILE_INCLUDE', 1);
+//define('GLOBAL_SYMBOL_REDECLARED_FUNCTION', 1);
+//define('GLOBAL_SYMBOL_REDECLARED_CLASS', 1);
 
 // define('PHPMCC_USED_FAST_PATH', 1);
 
@@ -83,107 +85,3 @@ define('GLOBAL_SYMBOL_REDECLARED_CLASS', 1);
 // function xhp_preprocess_code() { }
 
 //class phpmcc {}
-
-
-// define('MCC_DELETE_NOTFOUND', 1);
-// define('MCC_PROXY_GET_OP', 1);
-// 
-// define('MCC_IPPROTO_TCP', 1);
-// define('MCC_IPPROTO_UDP', 1);
-// 
-// define('MCC_ARG_MIRROR_CFG_NAME', 1);
-// define('MCC_ARG_MIRROR_CFG_MODEL', 1);
-// define('MCC_ARG_MIRROR_CFG_SERVERPOOLS', 1);
-// define('MCC_ARG_PERSISTENT', 1);
-// define('MCC_ARG_SERVERS', 1);
-// define('MCC_ARG_MIRROR_CFG', 1);
-// define('MCC_ARG_DEFAULT_PREFIX', 1);
-// define('MCC_ARG_FB_SERIALIZE_ENABLED', 1);
-// define('MCC_ARG_FB_SERIALIZE_PREFIXES', 1);
-// define('MCC_ARG_CONN_TMO', 1);
-// define('MCC_ARG_CONN_NTRIES', 1);
-// define('MCC_ARG_CONSISTENT_HASHING_PREFIXES', 1);
-// define('MCC_ARG_TMO', 1);
-// define('MCC_ARG_DGRAM_NTRIES', 1);
-// define('MCC_ARG_DGRAM_TMO_WEIGHT', 1);
-// define('MCC_ARG_UDP_REPLY_PORTS', 1);
-// 
-// define('MCC_HAVE_FB_SERIALIZATION', 1);
-// 
-// //??? facebook specific ?
-// define('MCC_GET_RECORD_ERRORS', 1);
-// define('MCC_DELETE_ERROR_LOG', 1);
-// define('MCC_DELETE_DELETED', 1);
-// 
-// define('MCC_SERVER_DOWN', 1);
-// 
-// define('MCC_ARG_SERVER_RETRY_TMO_MS', 1);
-// 
-// define('MCC_ARG_DGRAM_TMO_THRESHOLD', 1);
-// 
-// define('MCC_ARG_WINDOW_MAX', 1);
-// 
-// define('MCC_HAVE_ZLIB_COMPRESSION', 1);
-// 
-// define('MCC_ARG_COMPRESSION_THRESHOLD', 1);
-// 
-// define('MCC_ARG_NZLIB_COMPRESSION', 1);
-// 
-// define('MCC_ARG_PROXY', 1);
-// 
-// define('MCC_ARG_PROXY_OPS', 1);
-// 
-// define('MCC_PROXY_DELETE_OP', 1);
-// 
-// define('MCC_PROXY_ARITH_OP', 1);
-// 
-// define('MCC_PROXY_UPDATE_OP', 1);
-// 
-// define('MCC_ARG_NPOOLPREFIX', 1);
-// 
-// define('MCC_HAVE_DEBUG_LOG', 1);
-// 
-// define('MCC_ARG_DEBUG_LOGFILE', 1);
-// 
-// define('MCC_COMPRESSION_THRESHOLD', 1);
-// 
-// define('MCC_NZLIB_COMPRESSION', 1);
-// 
-// define('MCC_CONN_TMO_MS', 1);
-// 
-// define('MCC_CONN_NTRIES', 1);
-// 
-// define('MCC_ARG_DEBUG', 1);
-// 
-// 
-// define('MCC_DGRAM_NTRIES', 1);
-// 
-// define('MCC_DGRAM_TMO_WEIGHT', 1);
-// 
-// define('MCC_ARG_NODELAY', 1);
-// 
-// define('MCC_NODELAY', 1);
-// 
-// define('MCC_ARG_POLL_TMO', 1);
-// 
-// define('MCC_POLL_TMO_US', 1);
-// 
-// define('MCC_TMO_MS', 1);
-// 
-// define('MCC_UDP_REPLY_PORTS', 1);
-// 
-// define('MCC_WINDOW_MAX', 1);
-// 
-// define('MCC_ARG_TCP_INACTIVITY_TIME', 1);
-// 
-// define('MCC_TCP_INACTIVITY_TMO_DEFAULT', 1);
-// 
-// define('MCC_SERVER_RETRY_TMO_MS', 1);
-// 
-// define('MCC_DGRAM_TMO_THRESHOLD', 1);
-// 
-// define('MCC_POOLPREFIX_LEN', 1);
-// 
-// define('PHPMCC_USED_SLOW_PATH', 1);
-// 
-// define ('MCC_SERVER_UP', 1);
