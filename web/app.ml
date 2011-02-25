507a8eaacc2225f4d798ee4d7dda7f5178552a22
pad <pad@fb.com>
2011-02-24 20:00:58 -0800

    simple lxr clone prototype using ocsigen ! :)

--- web/app.ml
+++ web/app.ml
@@ -1,30 +1,86 @@
+open Common
+
+module Db = Database_php
+module H = XHTML.M
+
+module HC = Highlight_code
+
+let init () = 
+  Common_extra.set_link();
+  Database_php_storage.set_link();
+  ()
+
+let _ = init()
+(* todo: ugly, would be better to have access to this information from
+ * the ocsigen command line.
+ * Also ugly that have to put that globally so that the
+ * endpoint can access it :(
+ *)
+let db = !(Db._current_open_db_backend) "/tmp/pfff_db"
+
+let _ =
+  Sys.set_signal Sys.sigint (Sys.Signal_handle   (fun _ -> 
+    pr2 "C-c intercepted, will do some cleaning before exiting";
+    Db.close_db db
+  ))
+
+let htmlize_dir ~link dir db =
+  let subdirs = Common.readdir_to_dir_list dir +> Common.sort in
+  let files = Common.readdir_to_file_list dir +> Common.sort in
+  let files = files +> Common.exclude (fun file ->
+    let (d,b,e) = Common.dbe_of_filename_noext_ok file in
+    e = "git_annot" || e = "cm_cache"
+  )
+  in
+
+  let elements = subdirs ++ files in
+  (H.html (*~a:[H.a_xmlns `W3_org_1999_xhtml; H.a_xml_lang "en"]*)
+    (H.head
+        (H.title (H.pcdata "XHTML"))
+        [H.style ~contenttype:"text/css" [H.pcdata Htmlize_php2.style ]])
+    (H.body
+        ((H.h1 [H.pcdata dir] )
+          ::
+          (elements +> List.map (fun subelement ->
+            let fullpath = Filename.concat dir subelement in
+            let readable = Db.absolute_to_readable_filename fullpath db in
+            [(*H.h3 [H.pcdata subelement]; *)
+             H.h1 [
+               Eliom_output.Xhtml.a link [H.pcdata readable] readable;
+             ];
+             (* H.pre [H.pcdata readable]; *)
+            ]
+          ) +> List.flatten)
+        )
+    )
+  )
+
+
+let lxr = Eliom_services.service [""] (Eliom_parameters.string "path") ()
 
 (* from the eliom tutorial *)
-let coucou =
-  Eliom_output.Xhtml5.register_service
-    ~path:[""]
-    ~get_params:Eliom_parameters.unit
-    (fun () () ->
-      Lwt.return
-        (XHTML5.M.html
-          (XHTML5.M.head (XHTML5.M.title (XHTML5.M.pcdata "")) [])
-          (XHTML5.M.body [XHTML5.M.h1 [XHTML5.M.pcdata "Hallo!"]])))
+let _ = Eliom_output.Xhtml.register lxr
+    (fun readable_path () ->
+      (* todo? sanitized path ? *)
 
-(* with some open and a counter *)
-open XHTML5.M
-open Eliom_parameters
+      let path = Db.readable_to_absolute_filename readable_path db in
+      let hook_token s tok categ =
+        match categ with
+        | Some (HC.Function (HC.Use2 _)) ->
 
-let count =
-  let next =
-    let c = ref 0 in
-      (fun () -> c := !c + 1; !c)
-  in
-  Eliom_output.Xhtml5.register_service
-    ~path:["count"]
-    ~get_params:unit
-    (fun () () ->
-      Lwt.return
-        (html
-         (head (title (pcdata "counter")) [])
-         (body [p [pcdata (string_of_int (next ()))]])))
-
+            (try 
+              let id = Db.id_of_function s db in
+              let file = Db.readable_filename_of_id id db in
+              Eliom_output.Xhtml.a lxr [H.pcdata s] file
+            with (Not_found | Multi_found) as exn ->
+              Eliom_output.Xhtml.a lxr [H.pcdata s] (Common.exn_to_s exn)
+            )
+        | _ -> H.pcdata s
+      in
+      let html = 
+        if Common.is_directory path
+        then htmlize_dir ~link:lxr path db
+        else Htmlize_php2.htmlize_with_headers ~hook_token path db 
+      in
+      Lwt.return html
+    )
---
+++ web/htmlize_php2.ml
@@ -0,0 +1,178 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2011 Facebook
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
+
+
+open Common
+
+module Ast = Ast_php
+module Db = Database_php
+
+module H = XHTML.M
+
+module HC = Highlight_code
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
+(* 
+ * A better htmlizer, using semantic information ! 
+ *
+ * Right now I use the xHTML.ml module from Thorsten Ohl.
+ * The types can be quite tricky and XHTML.M above contain
+ * nested modules which makes it hard to follow. I hate modules!
+ * When pretty printing, especially when you want the pretty
+ * printer to respect the newlines you've put in the html element, you
+ * need to pass the ~preformatted label to the function.
+ * 
+ * todo? use xmlp4 ?
+ * 
+ * This is mostly copy paste of the one in lang_php/analyze/ but using
+ * the XHTML of ocsigen. Also do not need to pretty print the
+ * html as we return the typed html directly to the app.ml
+ *)
+
+(*****************************************************************************)
+(* Types *)
+(*****************************************************************************)
+
+(*****************************************************************************)
+(* Helpers *)
+(*****************************************************************************)
+
+(* todo: need typed CSS ! *)
+let style = "
+      body {
+        color: #f5deb3;
+        background-color: #2f4f4f;
+      }
+      .default {
+        /* default */
+        color: #f5deb3;
+        background-color: #2f4f4f;
+      }
+      a { color:#ccf; }
+"
+
+let decorate_pcdata_with_attributes pcdata attrs =
+  attrs |> List.fold_left (fun acc attr ->
+    match attr with
+    | `FOREGROUND s -> 
+        let (r,g,b) = Simple_color.rgb_of_string s in
+        let c = spf "#%02X%02X%02X" r g b in
+        H.span ~a:[H.a_style (spf "color: %s" c)]
+          [acc]
+          
+    | `SCALE scale ->
+        (* [> `LARGE | `MEDIUM | `XX_LARGE | `X_LARGE ] *)
+        acc
+    | `STRIKETHROUGH abool ->
+        acc
+    | `STYLE style ->
+        (*[> `ITALIC ] *)
+        acc
+    | `UNDERLINE style -> 
+        (* [> `DOUBLE | `SINGLE ] *)
+        acc
+    | `WEIGHT bold ->
+        (* [> `BOLD ] ] *)
+        acc
+    | `BACKGROUND s -> acc
+        
+  ) pcdata
+
+(*****************************************************************************)
+(* Main entry point *)
+(*****************************************************************************)
+
+(* Returns a list of Html elt, almost one for each token. When a token
+ * is a comment we even generate one element per line.
+ *)
+let htmlize ~hook_token filename db =
+
+  let ids = db.Db.file_to_topids#assoc filename in
+  let asts_and_toks = ids +> List.map (fun id -> 
+    id, 
+    db.Db.defs.Db.toplevels#assoc id,
+    db.Db.defs.Db.tokens_of_topid#assoc id
+  ) 
+  in
+  let prefs = Highlight_code.default_highlighter_preferences in
+
+  let inside_pre = 
+    asts_and_toks |> List.map (fun (id, ast, toks) ->
+
+      let h = Hashtbl.create 101 in
+
+      let empty_hentities = Hashtbl.create 0 in
+
+      Highlight_php.visit_toplevel 
+        ~tag:(fun info categ -> Hashtbl.add h info categ)
+        prefs  empty_hentities  (ast, toks);
+      
+      toks |> Common.map_filter (fun tok -> 
+        let info = Token_helpers_php.info_of_tok tok in
+        let s = Token_helpers_php.str_of_tok tok in
+
+        if not (Ast.is_origintok info)
+        then None
+        else begin
+
+        let categ = Common.hfind_option info h in
+        let attrs =
+          match categ with
+          | None -> []
+          | Some categ -> Highlight_code.info_of_category categ
+        in
+
+        (* old: decorate_pcdata_with_attributes (H.pcdata s) attrs *)
+
+        let xs = Common.lines_with_nl_either s in
+        let res = 
+          xs +> List.map (function
+          | Left s -> 
+              (* by default would return H.pcdata s, but
+               * could also be used to transform text into
+               * clickable elements, as in lxr
+               *)
+              let data = hook_token s tok categ in
+              decorate_pcdata_with_attributes data attrs
+          | Right () ->
+              (* no need for attributes for newlines *)
+              H.pcdata "\n"
+        )
+        in
+        Some res
+        end
+      ) |> List.flatten
+    ) |> List.flatten
+  in
+  inside_pre
+
+let htmlize_with_headers ~hook_token filename db = 
+  let inside_pre = htmlize ~hook_token filename db in
+
+  let html = 
+    (H.html (*~a:[H.a_xmlns `W3_org_1999_xhtml; H.a_xml_lang "en"]*)
+        (H.head
+            (H.title (H.pcdata "XHTML"))
+            [H.style ~contenttype:"text/css" [H.pcdata style ]])
+        (H.body
+            ([H.h1 [H.pcdata filename];
+              H.pre (inside_pre)
+            ])
+        ))
+  in
+  html
