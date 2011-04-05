468d10fdc6ff1bb6b6ba386fd7583b083c4ea944
pad <pad@fb.com>
2011-04-04 17:35:13 -0700

    * data/php_stdlib/interfaces.php: new file

---
+++ data/php_stdlib/interfaces.php
@@ -0,0 +1,13 @@
+<?php
+
+// http://php.net/manual/en/class.iteratoraggregate.php
+interface IteratorAggregate {
+}
+
+// http://php.net/manual/en/class.arrayaccess.php
+interface ArrayAccess {
+}
+
+// http://us2.php.net/manual/en/class.countable.php
+interface Countable {
+}
