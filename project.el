8f340dc98260242f7754c417b293f177315d5c16
pad <pad@fb.com>
2011-05-12 16:43:12 -0700

    * project.el: misc

--- project.el
+++ project.el
@@ -555,11 +555,12 @@
    (join-string 
     (list 
      "-debugger"
-     (case 3
+     (case 10
        (0 "-test")
        (1 "--flib-map /home/pad/www/flib/_bin/.flib.map /home/pad/www/flib/buffy/")
        (2 "--strict /home/pad/www/flib/site/tools/pfff/")
        (3 "-test_visualize_dependencies 1 /home/pad/pfff/facebook/tests/mini_www/flib/")
+       (10 "-test")
        )
      ))
    )
