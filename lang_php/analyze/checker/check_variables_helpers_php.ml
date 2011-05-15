7f8f4e4a5d0c5bcc8aa0546c85d4aae24d087724
pad <pad@fb.com>
2011-05-14 19:06:24 -0700

    check_variables_php.ml: split in env_check_php.ml and check_variables_helpers_php.ml

---
+++ lang_php/analyze/checker/check_variables_helpers_php.ml
@@ -0,0 +1,221 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2010 Facebook
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
+
+open Common
+
+open Ast_php
+module Ast = Ast_php
+module E = Error_php
+module S = Scope_code
+module Flag = Flag_analyze_php
+module V = Visitor_php
+module Ent = Entity_php
+
+(*****************************************************************************)
+(* Helpers *)
+(*****************************************************************************)
+
+(* for now return only local vars, not class var like self::$x *)
+let vars_used_in_any x =
+  V.do_visit_with_ref (fun aref -> { V.default_visitor with
+    V.kexpr = (fun (k, vx) x ->
+      match Ast.untype x with
+      | Lambda def -> 
+          (* stop here, do not recurse in but count the use(...) as vars used *)
+          def.l_use +> Common.do_option (fun (_tok, vars) ->
+            vars +> Ast.unparen +> Ast.uncomma +> List.iter (function
+            | LexicalVar (_is_ref, dname) ->
+                Common.push2 dname aref
+            );
+          )
+      | _ -> k x
+    );
+
+    V.klvalue = (fun (k,vx) x ->
+      match Ast.untype x with
+      | Var (dname, _scope) ->
+          Common.push2 dname aref
+
+      (* Used to have a bad representation for A::$a[e]
+       * It was parsed asa VQualifier(VArrayAccesS($a, e))
+       * but e could contains variable too !! so should actually
+       * visit the lval. But we also don't want to visit certain
+       * parts of lval.
+         * Introducing ClassVar fixed the problem.
+       * The qualifier should be with Var, just like what I do for name.
+       * 
+       * old: | VQualifier (qu, lval) -> ()
+       *)
+            
+      | _ -> 
+          k x
+    );
+  }) x
+    
+(* TODO: qualified_vars_in !!! *)
+
+(* the lval can be an array expression like 
+ * $arr[$v] = 2;  in which case we must consider
+ * only $arr in the list of assigned var, not $v.
+ * 
+ * old: let vars = vars_used_in_lvalue lval in
+ * 
+ *)
+let rec get_assigned_var_lval_opt lval = 
+
+  (match Ast.untype lval with
+  | Var (dname,  _) ->
+      Some dname
+  | VArrayAccess (lval, e) ->
+      get_assigned_var_lval_opt lval
+  (* TODO *)
+  | _ -> None
+  )
+
+(* update: does consider also function calls to function taking parameters via
+ * reference. Use global info.
+ *)
+let vars_assigned_in_any =
+  V.do_visit_with_ref (fun aref -> { V.default_visitor with
+    V.kexpr = (fun (k,vx) x ->
+      match Ast.untype x with
+      | Assign (lval, _, _) 
+      | AssignOp (lval, _, _) 
+          
+      | AssignRef (lval, _, _, _) 
+      | AssignNew (lval, _, _, _, _,  _) ->
+          
+          let vopt = get_assigned_var_lval_opt lval in
+          vopt |> Common.do_option (fun v -> Common.push2 v aref);
+          
+          (* recurse, can have $x = $y = 1 *)
+          k x
+      | _ -> 
+          k x
+    );
+    }
+  )
+
+let keyword_arguments_vars_in_any = 
+  V.do_visit_with_ref (fun aref -> { V.default_visitor with
+    V.kargument = (fun (k, vx) x ->
+      match x with
+      | Arg e ->
+          (match e with
+          | (Assign((Var(dname, _scope), tlval_4), i_5,
+                     _e),
+                   t_8) ->
+              Common.push2 dname aref;
+              k x
+          | _ ->
+              k x
+          )
+      | ArgRef _ -> ()
+    );
+  })
+
+(* maybe could be merged with vars_assigned_in but maybe we want
+ * the caller to differentiate between regular assignements
+ * and possibly assigned by being passed by ref
+ *)
+let vars_passed_by_ref_in_any ~find_entity = 
+  V.do_visit_with_ref (fun aref -> 
+    let params_vs_args params args = 
+
+      let params = params +> Ast.unparen +> Ast.uncomma_dots in
+      let args = 
+        match args with
+        | None -> []
+        | Some args -> args +> Ast.unparen +> Ast.uncomma 
+      in
+               
+      Common.zip_safe params args +> List.iter (fun (param, arg) ->
+                  
+        (match arg with
+        | Arg 
+            (Lv((Var(dname, _scope), tlval_49)), t_50) ->
+            if param.p_ref <> None
+            then
+              Common.push2 dname aref
+        | _ ->
+            ()
+        )
+      );
+    in
+
+  { V.default_visitor with
+    V.klvalue = (fun (k, vx) x ->
+      match Ast.untype x with
+      | FunCallSimple (name, args) ->
+          E.find_entity_and_warn ~find_entity (Ent.Function, name)
+          +> Common.do_option (fun id_ast ->
+            match id_ast with
+            | Ast_php.FunctionE def ->
+                params_vs_args def.f_params (Some args)
+            | _ -> raise Impossible
+          );
+          k x
+      | FunCallVar _ 
+      | StaticMethodCallSimple _
+      | MethodCallSimple _
+          -> 
+          (* TODO !!! *)
+          k x
+      | _ -> 
+          k x
+    );
+    V.kexpr = (fun (k, vx) x ->
+      match Ast.untype x with
+      | New (tok, class_name_ref, args) ->
+          (match class_name_ref with
+          | ClassNameRefStatic (ClassName name) ->
+              
+              E.find_entity_and_warn ~find_entity (Ent.Class, name)
+              +> Common.do_option (fun id_ast ->
+                match id_ast with
+                | Ast_php.ClassE def ->
+                    (try 
+                        let constructor_def = 
+                          Class_php.get_constructor def in
+                        params_vs_args constructor_def.m_params args
+
+                     with Not_found ->
+                       (* TODO: too many FP for now
+                       if !Flag.show_analyze_error
+                       then pr2_once (spf "Could not find constructor for: %s" 
+                                         (Ast.name name));
+                       *)
+                       ()
+                    );
+
+                | _ -> raise Impossible
+              );
+              k x
+
+          | ClassNameRefStatic (Self _ | Parent _) ->
+              (* TODO *)
+              k x
+
+          | ClassNameRefLateStatic _ ->
+              (* todo ? *)
+              k x
+          | ClassNameRefDynamic _ ->
+              (* todo ? *)
+              k x
+          )
+      | _ -> k x
+    );
+    }
+  )
---
+++ lang_php/analyze/checker/env_check_php.ml
@@ -0,0 +1,139 @@
+(* Yoann Padioleau
+ *
+ * Copyright (C) 2010 Facebook
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * version 2.1 as published by the Free Software Foundation, with the
+ * special exception on linking described in file license.txt.
+ * 
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
+ * license.txt for more details.
+ *)
+
+open Common
+
+open Ast_php
+module Ast = Ast_php
+module E = Error_php
+module S = Scope_code
+module Flag = Flag_analyze_php
+
+(*****************************************************************************)
+(* Prelude *)
+(*****************************************************************************)
+
+(*****************************************************************************)
+(* Types, constants *)
+(*****************************************************************************)
+
+(* the ref is for the number of uses *)
+type environment = 
+  (dname * (Scope_code.scope * int ref)) list list 
+
+let unused_ok_when_no_strict s = 
+  if !E.strict 
+  then false
+  else 
+    List.mem s 
+      ["res"; "retval"; "success"; "is_error"; "rs"; "ret";
+       "e"; "ex"; "exn"; (* exception *)
+      ]
+
+let unused_ok s =     
+   s =~ "_.*"
+  || s = "unused"
+  || s = "dummy"
+  || s =~ "ignored.*"
+
+  || unused_ok_when_no_strict s
+
+let fake_dname s = 
+  DName (s, Ast.fakeInfo s)
+
+(*****************************************************************************)
+(* Environment *)
+(*****************************************************************************)
+
+(* Looking up a variable in the environment. 
+ *  
+ * May need to also return the env at this place ??? xs::zs  below ?
+ * For instance when look for a parent class in scope, and 
+ * then search again the parent of this parent, should start
+ * from the scope at the parent place ?
+ * 
+ * But in fact it is worse than that because php is dynamic so 
+ * should look at parent chain of things at run-time!
+ * But maybe in practice for most of the codebase, people
+ * dont define complex recursive class or dynamic hierarchies so
+ * doing it statically in a naive way is good enough.
+ *)
+let rec lookup_env2 s env = 
+  match env with 
+  | [] -> raise Not_found
+  | []::zs -> lookup_env2 s zs
+  | ((a,b)::xs)::zs -> 
+      if Ast_php.dname a = s 
+      then b 
+      else lookup_env2 s (xs::zs)
+let lookup_env a b = 
+  Common.profile_code "CheckVar.lookup_env" (fun () -> lookup_env2  a b)
+
+
+let lookup_env_opt a b = 
+  Common.optionise (fun () -> lookup_env a b)
+
+(* This is ugly. Perhaps we should transform the environment to have
+ * either a dname or a name so that those 2 types of entities
+ * are clearly separated.
+ *)
+let rec lookup_env2_for_class s env = 
+  match env with 
+  | [] -> raise Not_found
+  | []::zs -> lookup_env2_for_class s zs
+  | ((a,b)::xs)::zs -> 
+      if Ast_php.dname a = s && fst b = S.Class
+      then b 
+      else lookup_env2_for_class s (xs::zs)
+
+let lookup_env_opt_for_class a b = 
+  Common.optionise (fun () -> lookup_env2_for_class a b)
+
+
+
+(*****************************************************************************)
+(* (Semi) Globals, Julia's style *)
+(*****************************************************************************)
+
+(* use a ref because we may want to modify it *)
+let (initial_env: environment ref) = ref [
+  Env_php.globals_builtins |> List.map (fun s ->
+    fake_dname s, (S.Global, ref 1)
+  )
+]
+
+(* opti: cache ? use hash ? *)
+let _scoped_env = ref !initial_env
+
+(* TODO use generic implem of Common ? *)
+let new_scope() = _scoped_env := []::!_scoped_env 
+let del_scope() = _scoped_env := List.tl !_scoped_env
+let top_scope() = List.hd !_scoped_env
+
+let add_in_scope namedef =
+  let (current, older) = Common.uncons !_scoped_env in
+  _scoped_env := (namedef::current)::older
+
+
+(* ------------------------------------------------------------ *)
+let add_binding2 k v  = 
+
+  if !Flag.debug_checker 
+  then pr2 (spf "adding binding %s" (Ast.string_of_info (Ast.info_of_dname k)));
+
+  add_in_scope (k, v) 
+
+let add_binding k v = 
+  Common.profile_code "CV.add_binding" (fun () -> add_binding2 k v)
