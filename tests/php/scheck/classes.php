f64fbb3fe2a6072cb07953404d89d58a34ffea1e
pad <pad@fb.com>
2011-05-15 14:14:21 -0700

    * tests/php/scheck/classes.php: more unit tests about undefined members

--- tests/php/scheck/classes.php
+++ tests/php/scheck/classes.php
@@ -31,3 +31,29 @@ class CB extends CA {
 //ERROR: undefined class
 class CC extends CUnknwown {
 }
+
+
+class TestUndefinedMembers {
+  private $member0;
+  protected $member1;
+  public function testMembers() {
+    $this->member0=0;
+    $this->member1=0;
+
+    //ERROR: use of undefined member
+    $this->member2=0;
+  }
+}
+
+class TestUndefinedMembers2 extends TestUndefinedMembers {
+  private $member2;
+  public function testMembers() {
+    $this->member1=0;
+    $this->member2=0;
+    //ERROR: undefined member
+    $this->member3=0;
+
+    //ERROR: undefined member (private)
+    $this->member0=0;
+  }
+}
