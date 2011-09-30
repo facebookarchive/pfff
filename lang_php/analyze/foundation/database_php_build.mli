dae24650dd2501446e818e437897c1d3ed80f314
pad <pad@fb.com>
2011-09-29 17:20:41 -0700

    * lang_php/analyze/foundation/database_php_build.mli: comments

--- lang_php/analyze/foundation/database_php_build.mli
+++ lang_php/analyze/foundation/database_php_build.mli
@@ -1,6 +1,7 @@
 (*s: database_php_build.mli *)
 
 (* in helpers now: val _errors: string list ref *)
+
 val max_phase: int
 
 (* main entry point, does the whole job *)
@@ -9,6 +10,7 @@ val create_db :
   ?db_support:Database_php.db_support ->
   ?phase:int -> 
   ?files:Common.filename list option ->
+  (* only if want to go to phase 4 *)
   annotate_variables_program:
     (find_entity:Entity_php.entity_finder option -> Ast_php.program -> unit)
     option ->
@@ -20,16 +22,16 @@ val create_db :
 val fast_create_db_mem_a_la_cpp: 
   ?phase:int -> Common.path list -> Database_php.database
 
-(* wrapper that makes it easier for some code to not depend on database_php *)
-val build_entity_finder: 
-  Database_php.database -> Entity_php.entity_finder
-
 (* helpers used also in unit_analyze_php.ml *)
 val db_of_files_or_dirs: 
   ?annotate_variables_program:
     (find_entity:Entity_php.entity_finder option -> Ast_php.program -> unit)
     option ->
   Common.path list -> Database_php.database
+
+(* wrapper that makes it easier for some code to not depend on database_php *)
+val build_entity_finder: 
+  Database_php.database -> Entity_php.entity_finder
 
 val actions: unit -> Common.cmdline_actions
 
