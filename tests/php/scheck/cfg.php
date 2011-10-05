b556a7dae7a0be81ff54958a07d57c13104abc1f
pad <pad@fb.com>
2011-10-05 16:08:35 -0700

    * tests/php/scheck/cfg.php: misc

--- tests/php/scheck/cfg.php
+++ tests/php/scheck/cfg.php
@@ -1,6 +1,6 @@
 <?php
 
-function test_cfg_return1() {
+function test_cfg_after_return() {
   
   $a = 1;
   echo $a;
@@ -9,14 +9,14 @@ function test_cfg_return1() {
   echo $a;
 }
 
-function test_cfg_exn_2() {
+function test_cfg_after_exn() {
   // we have a few code like this. It's an error but a less important one
   throw new Exception();
   //ERROR: dead statement
   return false;
 }
 
-function test_cfg_break3() {
+function test_cfg_break() {
   // we have a few code like this. It's an error but a less important one
   switch(1) {
     case 1:
@@ -28,7 +28,7 @@ function test_cfg_break3() {
   }
 }
 
-function test_cfg_switch4() {
+function test_cfg_switch() {
   switch(1) {
     case 1:
       return 2;
@@ -39,12 +39,12 @@ function test_cfg_switch4() {
   echo 1;
 }
 
-function test_cfg_doublecolon5() {
+function test_cfg_doublecolon() {
   //ERROR: less important too
   return 1;;
 }
 
-function test_cfg_ifdef_like6() {
+function test_cfg_ifdef_like() {
   return null;
   
   //ERROR: less important; people abuse return as some kind of #if 0
