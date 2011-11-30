be15dccb1c8777aa4787d4c30eec65f1d41afc14
pad <pad@fb.com>
2011-11-29 17:08:24 -0800

    * lang_php/analyze/foundation/abstract_interpreter_php.ml: unit tests work again

--- lang_php/analyze/foundation/abstract_interpreter_php.ml
+++ lang_php/analyze/foundation/abstract_interpreter_php.ml
@@ -123,7 +123,7 @@ and get_function_list env heap = function
 
 module Taint = struct
     let taint_mode = ref false
-    let taint_expr a b c d e = failwith "todo"
+    let taint_expr a b c d e = failwith "taint_expr: Todo"
     let fold_slist sl =
       List.fold_left (fun acc x ->
         match x with
@@ -131,11 +131,62 @@ module Taint = struct
         | _ -> acc
       ) (Vabstr Tstring) sl
     let check_danger a b c d e f = ()
-    module GetTaint = struct
-        let value heap x = failwith "todo"
-    end
     let binary_concat env heap v1 v2 path =
       Vabstr Tstring
+
+    module GetTaint = struct
+      exception Found of string list
+
+      let rec list f l =
+        match l with
+        | [] -> ()
+        | [x] -> f x
+        | x :: rl -> f x; list f rl
+            
+      let rec value path ptrs x =
+        match x with
+        | Vany
+        | Vnull -> ()
+        | Vtaint s -> raise (Found [s])
+        | Vabstr _ -> ()
+        | Vbool _
+        | Vint _ -> ()
+        | Vref s ->
+            let n = ISet.choose s in
+            if not (ISet.mem n path)
+            then
+              let path = ISet.union s path in
+              (try
+                  value path ptrs (IMap.find n ptrs);
+                with Not_found -> ()
+              )
+            else ()
+        | Vptr n when ISet.mem n path ->
+            let path = ISet.add n path in
+            value path ptrs (IMap.find n ptrs);
+        | Vptr _ -> ()
+        | Vfloat _ -> ()
+        | Vstring _ -> ()
+        | Vrecord m ->
+            let vl = SMap.fold (fun x y acc -> (x, y) :: acc) m [] in
+            list (fun (x, v) -> value path ptrs v) vl;
+        | Vobject m ->
+            let vl = SMap.fold (fun x y acc -> (x, y) :: acc) m [] in
+            list (fun (x, v) -> value path ptrs v) vl;
+        | Varray vl ->
+            list (value path ptrs) vl;
+        | Vaarray (v1, v2) ->
+            value path ptrs v1;
+            value path ptrs v2;
+        | Vmethod _ -> ()
+        | Vsum vl ->
+            list (value path ptrs) vl
+              
+      let value heap v =
+        try value ISet.empty heap.ptrs v; None with Found l -> Some l
+          
+    end
+
 end
 
 
