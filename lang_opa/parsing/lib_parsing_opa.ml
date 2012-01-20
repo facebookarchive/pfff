48dfa3aeb14a8f3b9ac2479690ecafda3e2603b1
pad <pad@fb.com>
2012-01-19 17:59:50 -0800

    * lang_opa/parsing/ast_opa.ml: estet

--- lang_opa/parsing/lib_parsing_opa.ml
+++ lang_opa/parsing/lib_parsing_opa.ml
@@ -31,13 +31,11 @@ module Flag = Flag_parsing_opa
 (*****************************************************************************)
 
 let find_opa_files_of_dir_or_files xs = 
-  Common.files_of_dir_or_files_no_vcs_nofilter xs 
-  +> List.filter (fun filename ->
-    let ftype = File_type.file_type_of_file filename in
-    match ftype with
+  Common.files_of_dir_or_files_no_vcs_nofilter xs +> List.filter (fun filename->
+    match File_type.file_type_of_file filename with
     | File_type.PL (File_type.Opa) -> true
     | _ -> false
-  ) |> Common.sort
+  ) +> Common.sort
 
 (*****************************************************************************)
 (* Extract infos *)
@@ -50,4 +48,3 @@ let find_opa_files_of_dir_or_files xs =
 (*****************************************************************************)
 (* AST helpers *)
 (*****************************************************************************)
-
