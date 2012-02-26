3964afc65d8ca5576a1e1c3da15a8d78ce248072
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-02-26 14:41:14 -0800

    * commons/parallel.mli:

--- commons/parallel.mli
+++ commons/parallel.mli
@@ -6,4 +6,7 @@ val parallel_map : ('a -> 'b) -> 'a list -> 'b list
 type 'a job = unit -> 'a
 type 'a jobs = ('a job) list
 
+(* This will create (List.length xs) forks *)
 val map_jobs: tasks:int -> 'a jobs -> 'a list
+(* This will create (tasks) forks *)
+val map_batch_jobs: tasks:int -> 'a jobs -> 'a list
