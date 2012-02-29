20f1362901093a6a60369609e679f31b1c5730ad
pad <pad@fb.com>
2012-02-28 17:34:23 -0800

    * lang_php/analyze/foundation/callgraph_php2.ml: union_graph

--- lang_php/analyze/foundation/callgraph_php2.ml
+++ lang_php/analyze/foundation/callgraph_php2.ml
@@ -35,11 +35,24 @@ type callgraph = (node, node Set_poly.t) Map_poly.t
 (* Main entry point *)
 (*****************************************************************************)
 
-let (add_graph: node -> node -> callgraph -> callgraph) =
+let (add_graph2: node -> node -> callgraph -> callgraph) =
  fun src target graph ->
   let vs = try Map_poly.find src graph with Not_found -> Set_poly.empty in
   let vs = Set_poly.add target vs in
   Map_poly.add src vs graph
+
+let add_graph a b c =
+  Common.profile_code "Callgraph.add" (fun () -> add_graph2 a b c)
+
+(* it's more efficient if g2 is the small graph compared to g1 *)
+let (union_graph2: callgraph -> callgraph -> callgraph) = fun g1 g2 ->
+  Map_poly.fold (fun k set1 acc ->
+    let set2 = try Map_poly.find k acc with Not_found -> Set_poly.empty in
+    Map_poly.add k (Set_poly.union set1 set2) acc
+  ) g2 g1
+
+let union_graph a b =
+  Common.profile_code "Callgraph.union" (fun () -> union_graph2 a b)
 
 (*****************************************************************************)
 (* string -> node, node -> string *)
--- lang_php/analyze/foundation/callgraph_php2.mli
+++ lang_php/analyze/foundation/callgraph_php2.mli
@@ -9,6 +9,8 @@ type node =
 type callgraph = (node, node Set_poly.t) Map_poly.t
 
 val add_graph: node -> node -> callgraph -> callgraph
+(* used as reduce step for the map-reduce computing the callgraph *)
+val union_graph: callgraph -> callgraph -> callgraph
 
 val string_of_node: node -> string
 val node_of_string: string -> node
