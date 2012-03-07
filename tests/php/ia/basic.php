5c2d67cd2b1fb3e55719265d618900e7bca6d037
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-03-06 23:28:00 -0800

    * tests/php/ia/: few test files

---
+++ tests/php/ia/basic.php
@@ -0,0 +1,9 @@
+<?php
+
+$x = 42;
+var_dump($x);
+$x = 2;
+var_dump($x);
+$y = $x;
+var_dump($y);
+
---
+++ tests/php/ia/foo.php
@@ -0,0 +1,10 @@
+<?php
+
+function main() {
+  $x = 1;
+  var_dump($x);
+  var_dump(2);
+  var_dump(2 + $x);
+}
+
+main();
