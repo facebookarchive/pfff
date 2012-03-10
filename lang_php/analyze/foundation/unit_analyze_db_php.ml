1cd3da989c7b94810de2fb71a971350b5a6cebe3
pad <pad@fb.com>
2012-03-09 16:12:43 -0800

    * lang_php/analyze/foundation/unit_analyze_db_php.ml:

--- lang_php/analyze/foundation/unit_analyze_db_php.ml
+++ lang_php/analyze/foundation/unit_analyze_db_php.ml
@@ -232,13 +232,13 @@ let lookup_unittest =
 
     "static lookup" >:: (fun () ->
       let file = "
-class A { static function a() { return A; } }
+class A { static function a() { return CST; } }
 " in
       let find_entity = entity_finder_from_string file in
       let def = Class_php.lookup_method ("A","a") find_entity in
       match def with
       | { m_body = MethodBody (_, 
-         [Stmt (Return (_, (Some (Sc (C (CName (Name ("A",_)))))), _))],
+         [Stmt (Return (_, (Some (Sc (C (CName (Name ("CST",_)))))), _))],
                               _); _ }
           -> ()
       | _ ->assert_failure "it should find simple static method"
