745f82a2b39d67773b9c289bf51ce4017c0b1811
pad <pad@fb.com>
2012-04-09 17:09:23 -0700

    * lang_cpp/parsing/export_ast_cpp.ml: indent

--- lang_cpp/parsing/unit_parsing_cpp.ml
+++ lang_cpp/parsing/unit_parsing_cpp.ml
@@ -18,46 +18,52 @@ let parse file =
 (*****************************************************************************)
 
 let unittest =
-  "parsing_cpp" >::: [
+ "parsing_cpp" >::: [
 
-    (*-----------------------------------------------------------------------*)
-    (* Lexing *)
-    (*-----------------------------------------------------------------------*)
-    (* todo: 
-     * - make sure parse int correctly, and float, and that actually does
-     *   not return multiple tokens for 42.42
-     * - ...
-     *)
+   (*-----------------------------------------------------------------------*)
+   (* Lexing *)
+   (*-----------------------------------------------------------------------*)
+   (* todo: 
+    * - make sure parse int correctly, and float, and that actually does
+    *   not return multiple tokens for 42.42
+    * - ...
+    *)
 
-    (*-----------------------------------------------------------------------*)
-    (* Parsing *)
-    (*-----------------------------------------------------------------------*)
-    "regression files" >:: (fun () ->
-      let dir = Filename.concat Config.path "/tests/cpp/parsing" in
-      let files = 
-        Common.glob (spf "%s/*.cpp" dir) ++ Common.glob (spf "%s/*.h" dir) in
-      files +> List.iter (fun file ->
-        try
-          let _ast = parse file in
-          ()
-        with Parse_cpp.Parse_error _ ->
-          assert_failure (spf "it should correctly parse %s" file)
-      )
-    );
+   (*-----------------------------------------------------------------------*)
+   (* Parsing *)
+   (*-----------------------------------------------------------------------*)
+   "regression files" >:: (fun () ->
+     let dir = Filename.concat Config.path "/tests/cpp/parsing" in
+     let files = 
+       Common.glob (spf "%s/*.cpp" dir) ++ Common.glob (spf "%s/*.h" dir) in
+     files +> List.iter (fun file ->
+       try
+         let _ast = parse file in
+         ()
+       with Parse_cpp.Parse_error _ ->
+         assert_failure (spf "it should correctly parse %s" file)
+     )
+   );
+   
+   "rejecting bad code" >:: (fun () ->
+     let dir = Filename.concat Config.path "/tests/cpp/parsing_errors" in
+     let files = 
+       Common.glob (spf "%s/*.cpp" dir) in
+     files +> List.iter (fun file ->
+       try 
+         let _ast = parse file in
+         assert_failure (spf "it should have thrown a Parse_error %s" file)
+       with
+       | Parse_cpp.Parse_error _ -> ()
+       | exn -> assert_failure (spf "throwing wrong exn %s on %s"
+                                   (Common.exn_to_s exn) file)
+     )
+   );
+   
+ (* todo: parsing C files (and not C++ files) containing C++ keywords 
+ *)
 
-    "rejecting bad code" >:: (fun () ->
-      let dir = Filename.concat Config.path "/tests/cpp/parsing_errors" in
-      let files = 
-        Common.glob (spf "%s/*.cpp" dir) in
-      files +> List.iter (fun file ->
-        try 
-          let _ast = parse file in
-          assert_failure (spf "it should have thrown a Parse_error %s" file)
-        with Parse_cpp.Parse_error _ ->
-          ()
-      )
-    );
-    (*-----------------------------------------------------------------------*)
-    (* Misc *)
-    (*-----------------------------------------------------------------------*)
-  ]
+ (*-----------------------------------------------------------------------*)
+ (* Misc *)
+ (*-----------------------------------------------------------------------*)
+ ]
