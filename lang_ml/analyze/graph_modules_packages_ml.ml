1749fd6eb04a3d405f34be9a244621578aaeb50f
pad <pad@fb.com>
2012-05-14 19:36:25 -0700

    * lang_ml/analyze/graph_modules_packages_ml.ml: move comments

--- lang_ml/analyze/graph_modules_packages_ml.ml
+++ lang_ml/analyze/graph_modules_packages_ml.ml
@@ -12,7 +12,6 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
  * license.txt for more details.
  *)
-
 open Common
 
 open Ast_ml
@@ -23,47 +22,11 @@ module G = Graph
 (* Prelude *)
 (*****************************************************************************)
 (*
- * Dependency visualization for ocaml code.
+ * Dependency visualization for OCaml code, the obsolete code.
+ * See main_codegraph.ml for discussions about alternatives and
+ * graph_code_ml.ml for the new way to visualize dependencies.
  * 
- * alternatives:
- *  - ocamldoc -dot ...
- *    But if there is one parse error or a module not found, then 
- *    ocamldoc fails. Also there is no package "projection" so it's 
- *    hard to apply on large projects. There is also no with-extern view
- *    with the slice of the graph to a directory.
- *    TODO It supports better code using camlp4 though.
- *  - ocamldoc with graphviz  
- *    graphviz does not do variable node size for free as in gephi
- *  - ocamldoc -dot-reduce ... 
- *    The -dot-reduce is good for layering, but sometimes
- *    it's also good to see things without the reduction (especially
- *    with gephi). See for instance the graph for tiger with ocamldoc
- *    vs pm_depend. I can see all the real callers to option.ml.
- *    TODO the reduce and layering is also useful ... try to do it in gephi too
- *  - ocamldoc -dot-colors
- *    TODO this is useful. 
- *    It's somehow covered by the strongly-connected + coloring in gephi.
- *  - graphviz backend? graphviz is good for layers, but
- *    you lose space because the high-level stuff is at the top but alone.
- *    With gephi/fatlas, by putting high-level stuff at the center, 
- *    you lose less space? Also graphviz does not scale very well.
- * 
- * todo? there is no edge weight? But is it useful in an ocaml context?
- * We can't have mutually dependent files or directories; the ocaml compiler
- * imposes a layering, so the in-edge will be enough information to give
- * more weight to some nodes. Thx to this layering the connected components
- * module of gephi also does some good work.
- * 
- * todo? if give edge weight, then need to modulate depending on
- * the type of the reference. 2 references to a function in another
- * module is more important than 10 references to some constructors.
- * Type|Exception > Function|Class|Global > Constructors|constants ?
- * 
- * todo: could annotate edges with the actual dependency
- * (a function name example, a type name example), that way maybe
- * in gephi we could see what an edge corresponds too (especially useful
- * with edge where we don't understand why there is a dependency).
- * 
+ * todo: move in old/
  *)
 
 (*****************************************************************************)
