46280bcd03c8d8e48b7503c4d3204c0990d10203
pad <pad@fb.com>
2012-06-18 17:22:21 -0700

    * lang_php/analyze/checker/check_all_php.ml: comments

--- lang_php/analyze/checker/check_all_php.ml
+++ lang_php/analyze/checker/check_all_php.ml
@@ -36,10 +36,10 @@ open Common
 let check_file ?(verbose=true) ?(find_entity=None) env file =
 
  Common.save_excursion Flag_analyze_php.verbose_checking verbose (fun() ->
-  (* we need to unsugar self/parent earlier now (we used to do it only
+  (* We need to unsugar self/parent earlier now (we used to do it only
    * before Check_functions_php) because check_and_annotate_program
    * needs to tag if something is passed by reference, which requires
-   * now to lookup static methods, which requires the self/parent unsugaring
+   * now to lookup static methods, which requires the self/parent unsugaring.
    * 
    * todo: unsugar traits by inlining/mixing their code in the class
    * using them.
@@ -50,7 +50,8 @@ let check_file ?(verbose=true) ?(find_entity=None) env file =
 
   (* even if find_entity=None, check_and_annotate_program can find
    * interesting bugs on local variables. There will be false positives
-   * though when variables are passed by reference.
+   * though when variables are passed by reference but it's better than
+   * nothing.
    *)
   Check_variables_php.check_and_annotate_program find_entity ast;
 
@@ -59,7 +60,7 @@ let check_file ?(verbose=true) ?(find_entity=None) env file =
   (* not ready yet: Check_dfg_php.check_program ?find_entity ast; *)
   Check_misc_php.check ast;
 
-  (* work only when have a find_entity; requires global view of the code *)
+  (* work only when have a find_entity; requires a global view of the code *)
   find_entity +> Common.do_option (fun find_entity ->
     Check_functions_php.check_program find_entity ast;
     Check_classes_php.check_program   find_entity ast;
