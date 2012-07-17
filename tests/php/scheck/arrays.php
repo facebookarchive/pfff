449ebdfe84de352ee871c072a35e2ca5af7f3eff
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-07-17 16:07:46 -0700

    * tests/php/scheck/arrays.php: yeah :) new code handle more cases

--- tests/php/scheck/arrays.php
+++ tests/php/scheck/arrays.php
@@ -3,8 +3,10 @@
 function test_undeclared_array($array) {
   $array['fld'] = 1;
 
+  //ERROR: use of undefined array
   $arrayy['fld2'] = 1;
 
   echo $array;
+  //ERROR: use of undefined array
   echo $arrayy;
 }
