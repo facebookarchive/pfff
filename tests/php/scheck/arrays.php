c8fa48877c2d1fc73ef4bf9ae0609ab5e3a78877
pad <pad@fb.com>
2012-08-16 16:16:14 -0700

    * tests/php/scheck/arrays.php: rename vars

--- tests/php/scheck/arrays.php
+++ tests/php/scheck/arrays.php
@@ -1,12 +1,13 @@
 <?php
 
-function test_undeclared_array($array) {
-  $array['fld'] = 1;
+function test_undeclared_array($array_parameter) {
+  $array_parameter['fld'] = 1;
 
   //ERROR: use of undefined array
-  $arrayy['fld2'] = 1;
+  $undefined_array['fld2'] = 1;
 
-  echo $array;
+  echo $array_parameter;
   //ERROR: use of undefined array
-  echo $arrayy;
+  echo $undefined_array;
 }
+
