78b18afd0e4d8fbee1960cc9de49dcadc583b7fa
pad <pad@fb.com>
2012-08-17 15:01:28 -0700

    * tests/php/scheck/arrays.php:

--- tests/php/scheck/arrays.php
+++ tests/php/scheck/arrays.php
@@ -3,7 +3,7 @@
 function test_undeclared_array($array_parameter) {
   $array_parameter['fld'] = 1;
 
-  //ERROR: use of undefined array
+  //ERROR: use of undefined array (this used to be ok in the old var checker)
   $undefined_array['fld2'] = 1;
 
   echo $array_parameter;
