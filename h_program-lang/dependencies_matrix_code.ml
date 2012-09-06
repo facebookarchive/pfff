170beb0cbb539ebe0b1c0f1ac194405e2fe51260
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-09-05 22:47:42 -0700

    * h_program-lang/dependencies_matrix_code.ml: naive partition algorithm, assume no cycle

--- h_program-lang/dependencies_matrix_code.ml
+++ h_program-lang/dependencies_matrix_code.ml
@@ -108,6 +108,80 @@ let build_with_nodes_order nodes g =
   dm
 
 
+(* 
+ * See http://dsmweb.org/dsmweb/en/understand-dsm/technical-dsm-tutorial/partitioning.html
+ * 
+ * note: sorting by the number of cells which you depend on is not
+ * enough. For instance let's say X depends on 3 cells, A, B, Y and Y 
+ * depends on 4: A, B, C, D. One could consider to put X
+ * upper in the matrix, but because X depends on Y, it's better to put
+ * Y upper.
+ * 
+ * todo: optimize? we redo some computations ... should memoize
+ * more? or just invert rows/columns in the original matrix?
+ * 
+
+ *)
+let partition_matrix nodes dm =
+
+  let m = dm.matrix +> Array.map Array.copy in
+  let n = Array.length m in
+  for i = 0 to n - 1 do
+    m.(i).(i) <- 0
+  done;
+
+  (* "1. Identify system elements (or tasks) that can be determined (or
+   * executed) without input from the rest of the elements in the matrix.
+   * Those elements can easily be identified by observing an empty column
+   * in the DSM. Place those elements to the left of the DSM. Once an
+   * element is rearranged, it is removed from the DSM (with all its
+   * corresponding marks) and step 1 is repeated on the remaining
+   * elements."
+   *)
+
+  (* "2.Identify system elements (or tasks) that deliver no
+   * information to other elements in the matrix. Those elements can
+   * easily be identified by observing an empty row in the DSM. Place
+   * those elements to the right of the DSM. Once an element is
+   * rearranged, it is removed from the DSM (with all its corresponding
+   * marks) and step 2 is repeated on the remaining elements."
+   *)
+  let rec step2 todo_nodes =
+    match todo_nodes with
+    | [] -> []
+    | x::xs ->
+        let elts_with_empty_row, rest =
+          todo_nodes +> List.partition (fun node ->
+            let i = Hashtbl.find dm.name_to_i node in
+            let cnt = ref 0 in
+            for j = 0 to n - 1 do
+              if m.(i).(j) > 0 then incr cnt
+            done;
+            !cnt = 0
+          )
+        in
+        pr2_gen elts_with_empty_row;
+        (match elts_with_empty_row with
+        (* cycle? not a layered system ... stop for now *)
+        | [] -> 
+            pr2 "CYCLE";
+            pr2_gen todo_nodes;
+            todo_nodes
+        | x::xs ->
+            elts_with_empty_row +> List.iter (fun node ->
+              let j = Hashtbl.find dm.name_to_i node in
+              (* set to 0 the corresponding cells in the matrix *)
+              for i = 0 to n - 1 do
+                m.(i).(j) <- 0
+              done;
+            );
+            elts_with_empty_row ++ step2 rest
+        )
+  in
+  let done_nodes_step1 = step2 nodes in
+  done_nodes_step1
+  
+
 
 let build config g =
   
@@ -121,32 +195,7 @@ let build config g =
 
   (* Now we need to reorder to minimize the number of dependencies in the
    * top right corner of the matrix.
-   * 
-   * todo: optimize? we redo some computations ... should memoize
-   * more? or just invert rows/columns in the original matrix?
-   * 
-   * note: sorting by the number of cells which you depend on is not
-   * enough. For instance let's say X depends on 3 cells, A, B, Y and Y 
-   * depends on 4: A, B, C, D. One could consider to put X
-   * upper in the matrix, but because X depends on Y, it's better to put
-   * Y upper.
-   * 
    *)
-  let nodes_with_scrore =
-    nodes +> List.map (fun n ->
-      let i = Hashtbl.find dm.name_to_i n in
-      (* pr2_gen (n, i, dm.matrix.(i)); *)
-      let cnt = ref 0 in
-      
-      dm.matrix.(i) +> Array.iteri (fun j e ->
-        if e > 0 && j <> i  then begin 
-          (* incr e ? *)
-          incr cnt;
-          end
-      );
-      n, !cnt
-    ) +> Common.sort_by_val_lowfirst
-  in
-  pr2_gen nodes_with_scrore;
-  build_with_nodes_order (nodes_with_scrore +> List.map fst) g
+  let nodes_reordered = partition_matrix nodes dm in
+  build_with_nodes_order nodes_reordered g
 
