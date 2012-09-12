0946073dc139130f3d657c25b6d0c0c5bb48ba52
pad <pad@fb.com>
2012-09-11 18:03:25 -0700

    * data/php_stdlib/builtins_json.idl.php:

--- data/php_stdlib/builtins_json.idl.php
+++ data/php_stdlib/builtins_json.idl.php
@@ -7,7 +7,9 @@ define('JSON_HEX_QUOT', 0);
 define('JSON_FORCE_OBJECT', 0);
 define('JSON_NUMERIC_CHECK', 0);
 define('JSON_UNESCAPED_SLASHES', 0);
+define('JSON_PRETTY_PRINT', 0);
 define('JSON_FB_LOOSE', 0);
 define('JSON_FB_UNLIMITED', 0);
+define('JSON_FB_EXTRA_ESCAPES', 0);
 function json_encode($value, $options = 0) { }
 function json_decode($json, $assoc = false, $options = 0) { }
