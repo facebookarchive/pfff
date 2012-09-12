148ef226ccbf73aa6873909ff16ea67c1b28739c
pad <pad@fb.com>
2012-09-11 18:45:00 -0700

    * data/php_stdlib/hphp.php:

---
+++ data/php_stdlib/hphp.php
@@ -0,0 +1,36 @@
+<?php
+
+// constants used in IDL itself
+const null_string = '';
+const null_variant = 0;
+const null_array = array();
+const null_object = 0;
+
+const INT_MAX = 0;
+
+
+//TODO: should be extracted from somewhere?
+const k_ENT_COMPAT = 0;
+const k_HPHP_TRIM_CHARLIST = 0;
+const k_OPENSSL_PKCS1_PADDING = 0;
+const k_OCI_DEFAULT = 0;
+const RAND_MAX = 0;
+const k_E_USER_NOTICE = 0;
+const k_INI_SCANNER_NORMAL = 0;
+const k_OPENSSL_ALGO_SHA1 = 0;
+const k_SEEK_SET = 0;
+const q_Collator_SORT_REGULAR = 0;
+
+const k_CURLVERSION_NOW = 0;
+const k_EXTR_OVERWRITE = 0;
+const k_E_ALL = 0;
+const k_FORCE_GZIP = 0;
+const k_GLOBAL_STATE_IGNORE = 0;
+const k_MW_TransparentOpacity = 0;
+const k_OCI_COMMIT_ON_SUCCESS = 0;
+const k_OCI_DTYPE_LOB = 0;
+const k_OPENSSL_CIPHER_RC2_40 = 0;
+const k_PKCS7_DETACHED = 0;
+
+const k_SQLT_AFC = 0;
+const k_STR_PAD_RIGHT = 0;
