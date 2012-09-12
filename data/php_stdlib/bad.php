2fb4a712153c909f568e2fb65b0271e6155bc8c8
pad <pad@fb.com>
2012-09-11 18:45:03 -0700

    * data/php_stdlib/bad.php:

---
+++ data/php_stdlib/bad.php
@@ -0,0 +1,12 @@
+<?php
+
+// people should not abuse the case insensivity of PHP
+
+// used in phabricator
+
+const True = 1;
+const False = 0;
+
+class STDClass { }
+
+function pathInfo($path, $opt = 15) { }
