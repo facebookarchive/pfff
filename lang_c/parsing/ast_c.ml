16cb7cd977e388d8fa7b7314f49c5f4e43d686c7
pad <pad@fb.com>
2012-09-14 11:15:26 -0700

    * lang_c/analyze/ast_c_simple.ml:

--- lang_c/parsing/ast_c.ml
+++ lang_c/parsing/ast_c.ml
@@ -1,9 +1,9 @@
 (* 
  * See lang_cpp/parsing/ast_cpp.ml.
- * There is no Concrete Syntax Tree for C. If you want
- * the AST see lang_c/analyze/ast_c_simple.ml which is build from
+ * 
+ * There is no Concrete Syntax Tree for C. If you want a real
+ * AST then see lang_c/analyze/ast_c_simple.ml which is build from
  * an ast_cpp.ml tree.
  * 
  *)
-
 type program = Ast_cpp.program
