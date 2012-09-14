4e9428915c985f3c1af0cc320f73b1ef1dcb02b7
pad <pad@fb.com>
2012-09-14 13:53:32 -0700

    * lang_c/analyze/ast_c_simple_build.ml:

--- lang_c/analyze/ast_c_simple_build.ml
+++ lang_c/analyze/ast_c_simple_build.ml
@@ -40,9 +40,50 @@ let empty_env () = ()
 (* Helpers *)
 (*****************************************************************************)
 
+let todo any =
+  let _ii = Lib_parsing_cpp.ii_of_any any in
+  pr2 (Export_ast_cpp.ml_pattern_string_of_any any);
+  raise Todo
+
+
 (*****************************************************************************)
 (* Main entry point *)
 (*****************************************************************************)
 
-let program x =
-  raise Todo
+let rec program x =
+  let env = empty_env () in
+
+  List.map (toplevel env) x +> List.flatten
+
+and toplevel env = function
+  | CppTop x -> cpp_directive env x
+
+  | FinalDef _ -> []
+  (* not much we can do here, at least the parsing statistics should warn the
+   * user that some code was not processed
+   *)
+  | NotParsedCorrectly _ -> []
+
+  | x -> todo (Toplevel x)
+
+and cpp_directive env = function
+  | Define (tok, name, def_kind, def_val) as x ->
+      (match def_kind, def_val with
+      | DefineVar, DefineExpr e ->
+          [A.Define (name, expr env e)]
+      | _ -> todo (Cpp x)
+      )
+  | x -> todo (Cpp x)
+
+
+and expr env e =
+  let (e', toks) = e in
+  match e' with
+  | C cst -> constant env toks cst
+  | _ -> todo (Expr e)
+
+and constant env toks x = 
+  match x, toks with
+  | Int s, [x] -> A.Int (s,x)
+  | _ -> todo (Constant x)
+
