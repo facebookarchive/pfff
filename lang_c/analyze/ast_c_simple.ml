c97ca41ecd75a460d11a0d4ddc54fc8bb5ae5079
pad <pad@fb.com>
2012-09-14 13:53:39 -0700

    * lang_c/analyze/ast_c_simple.ml: first draft

--- lang_c/analyze/ast_c_simple.ml
+++ lang_c/analyze/ast_c_simple.ml
@@ -12,6 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the file
  * license.txt for more details.
  *)
+open Common.Infix
 
 (*****************************************************************************)
 (* Prelude *)
@@ -36,7 +37,7 @@
  *    NotParsedCorrectly. The only token information kept is for identifiers
  *    for error reporting. See name below.
  *  - ...
- *  - no nested struct, they are lifter at the toplevel
+ *  - no nested struct, they are lifted to the toplevel
  *  - no mix of typedef with decl
  *  - sugar is removed, no RecordAccess vs RecordPtAccess, ...
  * 
@@ -61,8 +62,21 @@ type name = string wrap
 (* Types *)
 (* ------------------------------------------------------------------------- *)
 
+(* todo: qualifier *)
 type type_ =
-  | TBuiltin of name
+  | TBase of name
+  | TPointer of type_
+  | TArray of type_
+  | TFunction of function_type
+  | TStructName of name
+  | TTypeName of name
+
+ and function_type = (type_ * parameter list)
+
+  and parameter = {
+    p_type: type_;
+    p_name: name;
+  }
 
 (* ------------------------------------------------------------------------- *)
 (* Expression *)
@@ -71,17 +85,63 @@ type expr =
   | Int of string wrap
   | Float of string wrap
   | String of string wrap
+  | Char of string wrap
 
   (* can be a cpp constant XXX, or a variable, or a function *)
   | Id of name
 
   | Call of expr * expr list
 
+  | Assign of Ast_cpp.assignOp option * expr * expr
+
+  | ArrayAccess of expr * expr
+  | RecordAccess of expr * name
+
+  | Cast of type_ * expr
+
+  (* todo? transform into Call (builtin ...) ? *)
+  | Postfix of expr * Ast_cpp.fixOp
+  | Infix of expr * Ast_cpp.fixOp
+  | Unary of expr * Ast_cpp.unaryOp
+  | Binary of expr * Ast_cpp.binaryOp * expr
+
+  | CondExpr of expr * expr * expr
+  (* should be a statement *)
+  | Sequence of expr * expr
+
 (* ------------------------------------------------------------------------- *)
 (* Statement *)
 (* ------------------------------------------------------------------------- *)
 type stmt = 
   | Expr of expr
+  | Block of stmt list
+
+  | If of expr * stmt * stmt
+  | Switch of expr * case list
+
+  | While of expr * stmt
+  | DoWhile of stmt * expr
+  | For of expr option * expr option * expr option * stmt
+
+  | Return of expr option
+  | Continue | Break
+
+  | Label of name * stmt
+  | Goto of name
+
+  | Decl of var_decl
+
+  and case =
+    | Case of expr * stmt list
+    | Default of stmt list
+
+and var_decl = {
+  v_name: name;
+  v_type: type_;
+  (* todo *)
+  v_storage: unit;
+  v_init: expr option;
+}
 
 (* ------------------------------------------------------------------------- *)
 (* Definitions *)
@@ -90,39 +150,41 @@ type stmt =
 type func_def = {
   f_name: name;
   f_body: stmt list;
+  f_type: function_type;
 }
-  and parameter = {
-    p_type: type_;
-    p_name: name;
-  }
 
 type struct_def = {
   s_name: name;
+  s_kind: struct_kind;
   s_flds: field_def list;
 }
+  and struct_kind = Struct | Union
+
   (* todo? merge with var_decl? *)
   and field_def = { 
     fld_name: name;
     fld_type: type_;
   }
 
-type var_decl = {
-  v_name: name;
-  v_type: type_;
-}
+(* ------------------------------------------------------------------------- *)
+(* Cpp *)
+(* ------------------------------------------------------------------------- *)
+type define_body = expr
 
 (* ------------------------------------------------------------------------- *)
 (* Program *)
 (* ------------------------------------------------------------------------- *)
 type toplevel =
-  (* no Ifdef *)
-  | Define of name * expr (* should be a statically computable expr *)
+  | Define of name * define_body (* should be a statically computable expr *)
   | Include of string wrap (* path *)
+  | Macro of name * (name list) * define_body
 
   | StructDef of struct_def
+  | TypeDef of name * type_
+
   | FuncDef of func_def
   | Global of var_decl
-  | TypeDef of name * type_
+
 
 type program = toplevel list
 
@@ -133,3 +195,9 @@ type program = toplevel list
 (*****************************************************************************)
 (* Helpers *)
 (*****************************************************************************)
+
+let str_of_name (s, _) = s
+
+let looks_like_macro name =
+  let s = str_of_name name in
+  s =~ "[A-Z][A-Z_0-9]*"
