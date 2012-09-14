541a5bdda98649b027315fb06292fb2a69b0657c
pad <pad@fb.com>
2012-09-14 14:53:09 -0700

    * lang_c/analyze/ast_c_simple_build.ml: more, also CplusplusConstruct patterns

--- lang_c/analyze/ast_c_simple_build.ml
+++ lang_c/analyze/ast_c_simple_build.ml
@@ -29,6 +29,7 @@ module A = Ast_c_simple
 (*****************************************************************************)
 
 exception ObsoleteConstruct of string * Ast_cpp.info
+exception CplusplusConstruct
 exception TodoConstruct of string * Ast_cpp.info
 
 (* not used for now *)
@@ -40,11 +41,9 @@ let empty_env () = ()
 (* Helpers *)
 (*****************************************************************************)
 
-let todo any =
+let rec debug any =
   let _ii = Lib_parsing_cpp.ii_of_any any in
-  pr2 (Export_ast_cpp.ml_pattern_string_of_any any);
-  raise Todo
-
+  pr2 (Export_ast_cpp.ml_pattern_string_of_any any)
 
 (*****************************************************************************)
 (* Main entry point *)
@@ -52,38 +51,115 @@ let todo any =
 
 let rec program x =
   let env = empty_env () in
-
   List.map (toplevel env) x +> List.flatten
+
+(* ---------------------------------------------------------------------- *)
+(* Toplevels *)
+(* ---------------------------------------------------------------------- *)
 
 and toplevel env = function
   | CppTop x -> cpp_directive env x
 
+  | Func (func_or_else) as x ->
+      (match func_or_else with
+      | FunctionOrMethod def ->
+          [func_def env def]
+      | Constructor _ | Destructor _ ->
+          debug (Toplevel x); raise CplusplusConstruct
+      )
+
+  | EmptyDef _ -> []
   | FinalDef _ -> []
+
+  | NameSpaceAnon (_, _)|NameSpaceExtend (_, _)|NameSpace (_, _, _) 
+  | ExternCList (_, _, _)|ExternC (_, _, _)|TemplateSpecialization (_, _, _)
+  | TemplateDecl _
+      as x ->
+      debug (Toplevel x); raise CplusplusConstruct
+      
+  | (MacroVarTop (_, _)|MacroTop (_, _, _)|IfdefTop _|BlockDecl _|DeclTodo) 
+      as x ->
+      debug (Toplevel x);
+      raise Todo
+
   (* not much we can do here, at least the parsing statistics should warn the
    * user that some code was not processed
    *)
   | NotParsedCorrectly _ -> []
 
-  | x -> todo (Toplevel x)
+(* ---------------------------------------------------------------------- *)
+(* Functions *)
+(* ---------------------------------------------------------------------- *)
+and func_def env def =
+  raise Todo
 
+(* ---------------------------------------------------------------------- *)
+(* Cpp *)
+(* ---------------------------------------------------------------------- *)
+  
 and cpp_directive env = function
   | Define (tok, name, def_kind, def_val) as x ->
       (match def_kind, def_val with
       | DefineVar, DefineExpr e ->
           [A.Define (name, expr env e)]
-      | _ -> todo (Cpp x)
+      | _ -> debug (Cpp x); raise Todo
       )
-  | x -> todo (Cpp x)
+  | Include (tok, inc_file) as x ->
+      let s =
+        (match inc_file with
+        | Local xs -> "\"" ^ Common.join "/" xs ^ "\""
+        | Standard xs -> "<" ^ Common.join "/" xs ^ ">"
+        | Wierd s -> 
+            debug (Cpp x); raise Todo
+        )
+      in
+      [A.Include (s, tok)]
+  | (PragmaAndCo _|Undef _) as x ->
+      debug (Cpp x); raise Todo
 
+(* ---------------------------------------------------------------------- *)
+(* Stmt *)
+(* ---------------------------------------------------------------------- *)
+
+and stmt env x =
+  raise Todo
+
+(* ---------------------------------------------------------------------- *)
+(* Expr *)
+(* ---------------------------------------------------------------------- *)
 
 and expr env e =
   let (e', toks) = e in
   match e' with
   | C cst -> constant env toks cst
-  | _ -> todo (Expr e)
+  | (TypeIdOfType (_, _)|TypeIdOfExpr (_, _)|GccConstructor (_, _)
+  | StatementExpr _|
+Cast (_, _)|SizeOfType (_, _)|SizeOfExpr (_, _)|RecordPtStarAccess (_, _)|
+RecordStarAccess (_, _)|RecordPtAccess (_, _)|RecordAccess (_, _)|
+ArrayAccess (_, _)|Binary (_, _, _)|Unary (_, _)|Infix (_, _)|Postfix (_, _)|
+Assignment (_, _, _)|Sequence (_, _)|CondExpr (_, _, _)|FunCallExpr (_, _)|
+FunCallSimple (_, _)|Ident (_, _)|ExprTodo) ->
+      debug (Expr e); raise Todo
+
+  | Throw _|DeleteArray (_, _)|Delete (_, _)|New (_, _, _, _, _)
+  | CplusplusCast (_, _, _)
+  | ConstructedObject (_, _) | This _
+      ->
+      debug (Expr e); raise CplusplusConstruct
+
+  | ParenExpr (_, e, _) -> expr env e
 
 and constant env toks x = 
   match x, toks with
   | Int s, [x] -> A.Int (s,x)
-  | _ -> todo (Constant x)
+  | (Bool _|Float _|Char _|String _|MultiString), _ -> 
+      debug (Constant x); raise Todo
+  | _, _ -> 
+      debug (Constant x); raise Impossible
+
+(* ---------------------------------------------------------------------- *)
+(* Misc *)
+(* ---------------------------------------------------------------------- *)
+
+
 
--- lang_c/analyze/ast_c_simple_build.mli
+++ lang_c/analyze/ast_c_simple_build.mli
@@ -1,5 +1,6 @@
 
 exception ObsoleteConstruct of string * Ast_cpp.info
+exception CplusplusConstruct
 exception TodoConstruct of string * Ast_cpp.info
 
 val program: 
