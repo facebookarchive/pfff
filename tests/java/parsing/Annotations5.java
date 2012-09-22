8f77608a1707b6f2bb3242349c6e1f92bf7967f0
pad <pad@fb.com>
2012-09-21 19:09:21 -0700

    * tests/java/parsing/: some todos

---
+++ tests/java/parsing/Annotations5.java
@@ -0,0 +1,12 @@
+
+/*
+BAD:!!!!! @RunWith(WithTestDefaultsRunner.class)
+BAD:!!!!! @Implements(PreferenceCategory.class)
+bad: public class ShadowPreferenceCategory extends ShadowPreferenceGroup {
+bad: @Implements(AssetManager.class)
+bad: public final class ShadowAssetManager {
+BAD:!!!!! @Implements(SQLiteQueryBuilder.class)
+BAD:!!!!!   @JMAutogen.ListType(jsonFieldName="edges", listElementTypes={FeedStoryEdge.class})
+BAD:!!!!!   @Test(expected = IllegalStateException.class)
+bad:     public void testIllegal() {
+*/
