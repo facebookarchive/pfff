c5af3c17cc18dc5ad792b419f1c0a6df63ae167f
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-09-22 10:36:03 -0700

    * tests/java/parsing/Annotations5.java: support

--- tests/java/parsing/Annotations5.java
+++ tests/java/parsing/Annotations5.java
@@ -1,12 +1,19 @@
+class X {
 
-/*
-BAD:!!!!! @RunWith(WithTestDefaultsRunner.class)
-BAD:!!!!! @Implements(PreferenceCategory.class)
-bad: public class ShadowPreferenceCategory extends ShadowPreferenceGroup {
-bad: @Implements(AssetManager.class)
-bad: public final class ShadowAssetManager {
-BAD:!!!!! @Implements(SQLiteQueryBuilder.class)
-BAD:!!!!!   @JMAutogen.ListType(jsonFieldName="edges", listElementTypes={FeedStoryEdge.class})
-BAD:!!!!!   @Test(expected = IllegalStateException.class)
-bad:     public void testIllegal() {
-*/
+ @RunWith(WithTestDefaultsRunner.class)
+ @Implements(PreferenceCategory.class)
+ void main() {
+ }
+
+ @Implements(AssetManager.class)
+ void main() {
+ }
+
+
+ @Implements(SQLiteQueryBuilder.class)
+ @JMAutogen.ListType(jsonFieldName="edges", listElementTypes={FeedStoryEdge.class})
+ @Test(expected = IllegalStateException.class)
+ void main() {
+ }
+
+}
