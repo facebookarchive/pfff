351b3d2a68d80d44fb8674a127d271ec38ba4f22
pad <pad@fb.com>
2012-10-29 16:38:01 -0700

    * lang_java/analyze/package_java.ml: moved code there

--- lang_java/analyze/graph_code_java.ml
+++ lang_java/analyze/graph_code_java.ml
@@ -202,60 +202,6 @@ let rec add_use_edge env (name, kind) =
 (* Class/Package Lookup *)
 (*****************************************************************************)
 
-let (lookup_fully_qualified2: 
-  Graph_code.graph -> string list -> Graph_code.node option) = 
- fun g xs ->
-  let rec aux current xs =
-    match xs with
-    | [] -> Some current
-    | x::xs ->
-        let children = G.children current g in
-        (* because have File intermediate (noisy) nodes *)
-        let children = children +> List.map (fun child ->
-          match child with
-          | (_, E.File) -> G.children child g
-          (* we prefer Package to Dir when we lookup, we don't want
-           * The "multiple entities" warning when have both
-           * a "net" package and "net" directory.
-           *)
-          | (_, E.Dir) -> []
-          | _ -> [child]
-        ) +> List.flatten
-        in
-        (* sanity check, quite expansive according to -profile *)
-        Common.group_assoc_bykey_eff children +> List.iter (fun (k, xs) ->
-          if List.length xs > 1 
-             (* issue warnings lazily, only when the ambiguity concerns
-              * something we are actually looking for 
-              *) 
-             && k =$= x
-          then begin
-            (* todo: this will be a problem when go from class-level
-             * to method/field level dependencies
-             *)
-            pr2 "WARNING: multiple entities with same name";
-            pr2_gen (k, xs);
-          end
-        );
-        
-        let str =
-          match current with
-          | ".", E.Dir -> x
-          | s, _ -> s ^ "." ^ x
-        in
-        let new_current = 
-          children +> Common.find_some_opt (fun (s2, kind) ->
-            if str =$= s2
-            then Some (s2, kind)
-            else None
-          ) in
-        (match new_current with
-        (* less: could return at least what we were able to resolve *)
-        | None -> None
-        | Some current -> aux current xs
-        )
-  in
-  aux G.root xs
 
 let _hmemo = Hashtbl.create 101 
 
@@ -264,9 +210,9 @@ let lookup_fully_qualified_memoized env x =
     if env.phase = Uses || env.phase = Inheritance
     then 
       Common.memoized _hmemo x (fun () ->
-        lookup_fully_qualified2 env.g x
+        Package_java.lookup_fully_qualified2 env.g x
       )
-    else lookup_fully_qualified2 env.g x
+    else Package_java.lookup_fully_qualified2 env.g x
   )
 
 (* Java allows to open namespaces by for instance importing packages
