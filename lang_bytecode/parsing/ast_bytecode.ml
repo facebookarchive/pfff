8f41924b7e9754fe660d21261fcdd2ec226e3f8a
pad <pad@fb.com>
2012-11-07 17:38:59 -0800

    * lang_bytecode/parsing/ast_bytecode.ml: comments

--- lang_bytecode/parsing/ast_bytecode.ml
+++ lang_bytecode/parsing/ast_bytecode.ml
@@ -17,11 +17,25 @@
 (* Prelude *)
 (*****************************************************************************)
 (* 
- * This is mostly a wrapper around javalib (see externals/javalib).
+ * This is mostly a wrapper around javalib (see external/javalib).
  * 
  * Pursuing both the source-code (lang_java/) and byte-code (lang_bytecode/)
- * path allows us to double check each other and avoid closing doors 
+ * paths allows us to double check each other and avoid closing doors 
  * for ourselves depending on the task.
+ * 
+ * Getting some simple analysis on the bytecode, e.g. graph_code_bytecode.ml
+ * is also a good first step towards using more advanced Java static
+ * analysis tools like Jchord. Indeed getting familiar with the
+ * bytecode format used by this tools is good to understand their limitations,.
+ * Moreover if codegraph on a codebase does not generate any error
+ * we are sure we have all the necessary source and are ready to run
+ * other tools.
+ * 
+ * The bytecode also offers a simple baseline to compare with if we do stuff
+ * at the source code level later. Indeed when we have mistakes at
+ * the source code level, it is good to know if we have the same problem
+ * at the bytecode level, in which case it's probably because some
+ * code was not included or on the opposite code should not be analyzed.
  *)
 
 
