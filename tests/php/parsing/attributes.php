c5e5ef3d98d53984b623081498ebf86806f4afc9
pad <pad@fb.com>
2012-11-20 14:41:32 -0800

    * tests/php/parsing/attributes_complex.php: split

--- tests/php/parsing/attributes.php
+++ tests/php/parsing/attributes.php
@@ -10,14 +10,6 @@ class MockD extends A { public function foo() { /* .. */ } }
 << __attribute >>
 function foo() { }
 
-// User attributes are denoted by double angle brackets (<< >>) before a
-// function or class definition. One or more attributes can be listed between
-// the angle brackets, separated by commas. An attribute is a name followed
-// optionally by a list of scalar values
-
-<< Foo, Bar(), Baz('hello',array(1,2)) >>
-function f() { /* .. */ }
-
 // Multiple attributes with the same name are not allowed. The scalar values
 // in attribute expressions are not allowed to contain user defined constants.
 // User attributes can be applied to classes and functions and methods, but
