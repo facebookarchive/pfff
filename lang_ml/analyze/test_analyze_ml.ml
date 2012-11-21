1809ed85673ddca6e8005dc898bd439edb199a20
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-11-20 19:43:27 -0800

    * lang_ml/analyze/meta_ast_cmt.ml: -dump_cmt2

--- lang_ml/analyze/test_analyze_ml.ml
+++ lang_ml/analyze/test_analyze_ml.ml
@@ -8,7 +8,12 @@ let dump_cmt file =
 
 let dump_cmt2 file =
   let info = Cmt_format.read_cmt file in
-  pr2_gen info.Cmt_format.cmt_annots
+  match info.Cmt_format.cmt_annots with
+  | Cmt_format.Implementation x ->
+      let v = Meta_ast_cmt.vof_structure x in
+      let str = Ocaml.string_of_v v in
+      pr str
+  | _ -> raise Todo
 
 let actions () = [
   "-dump_cmt", " <file>",
--- tests/ml/cmt/foo.ml
+++ tests/ml/cmt/foo.ml
@@ -1,2 +1,8 @@
+open List
+
 let a = 1
-let b x = x
+let b x y = x + y
+
+let c = List.map (fun x -> x) [1;2]
+let d = map (fun x -> x) [3;4]
+
