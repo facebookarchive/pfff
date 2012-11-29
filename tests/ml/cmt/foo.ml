8892f9e1baa6bb9ae8029ce289b1d06d8233e813
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-11-29 12:46:22 -0800

    * tests/ml/cmt/foo.ml: variant

--- tests/ml/cmt/foo.ml
+++ tests/ml/cmt/foo.ml
@@ -15,6 +15,14 @@ let use_global () =
 let use_hglobal () = 
   Hashtbl.add hglobal 1 true
 
+type variant =
+  | Constructor1
+  | Constructor2WithArg of int
+
+let use_variant = function
+  | Constructor1 -> 1
+  | Constructor2WithArg _ -> 2
+
 exception Error1
 
 module X = struct
