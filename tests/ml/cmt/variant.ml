fa83b6c0ffd3e533dbcd9744e840127efa4af1e4
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-11-29 17:56:24 -0800

    * tests/ml/cmt/variant.ml: unused variant

--- tests/ml/cmt/variant.ml
+++ tests/ml/cmt/variant.ml
@@ -1,7 +1,10 @@
 type variant =
   | Constructor1
   | Constructor2WithArg of int
+  | ConstructorUnused
 
 let use_variant = function
   | Constructor1 -> 1
   | Constructor2WithArg _ -> 2
+  | _ -> 3
+
