9cb31c74c504218f219dc3d383b6f51ed4e374e8
pieter@fb.com <pieter@fb.com>
2012-12-05 17:03:45 -0800

    Added link to the stackoverflow thread
    Summary: title
    
    Test Plan: comment... in a testcase
    
    Reviewed by: pad

--- tests/java/parsing/NewQualified.java
+++ tests/java/parsing/NewQualified.java
@@ -1,10 +1,12 @@
+// See also: http://stackoverflow.com/questions/2863157/how-does-object-new-work-does-java-have-a-new-operator
+
 public class NewQualified {
   public class Inner {
     private int x;
     public Inner(int x) {
       this.x = x;
     }
-    // clowntown here
+
     public int getX() {
       return this.x;
     }
