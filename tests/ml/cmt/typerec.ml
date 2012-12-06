7a650cd016e462c307a6217eb3662dacc01f6c08
Yoann Padioleau <yoann.padioleau@Padioleaumbp15.local>
2012-12-05 17:26:36 -0800

    * tests/ml/cmt/typerec.ml: more

--- tests/ml/cmt/typerec.ml
+++ tests/ml/cmt/typerec.ml
@@ -4,3 +4,15 @@ and bar = int
 type x = Foo of int list1
 
  and 'a list1 = 'a * 'a list
+
+
+type filename = string
+
+type fullid = filepos
+ and filepos = {
+   file: filename;
+ }
+
+(* not enough to expose the bug ... *)
+let f x =
+  x.file
