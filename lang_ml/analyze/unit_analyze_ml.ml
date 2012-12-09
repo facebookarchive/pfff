b0c3ae88f1c88188ddcb717c1007da7defe9be43
pad <pad@fb.com>
2012-12-09 15:01:17 -0800

    [codequery] basic unit test infrastructure for prolog queries on ml(.cmt) code
    Summary: This should make it easier to test things in the future.
    
    Test Plan:
    $ ./pfff_test prolog_ml
    $ ./pfff_test all
    
    Reviewers: pieter
    
    Reviewed By: pieter
    
    CC: julienv
    
    Differential Revision: https://phabricator.fb.com/D652940

--- lang_ml/analyze/unit_analyze_ml.ml
+++ lang_ml/analyze/unit_analyze_ml.ml
@@ -14,6 +14,51 @@ open OUnit
 
 let verbose = false
 
+let prolog_query ~files query =
+
+  let tmp_dir = Filename.temp_file (spf "prolog_ml-%d" (Unix.getpid())) "" in
+  Unix.unlink tmp_dir;
+  (* who cares about race *)
+  Unix.mkdir tmp_dir 0o755;
+  Common.finalize (fun () ->
+
+    (* generating cmt files *)
+    files +> List.iter (fun (filename, content) ->
+      Common.write_file ~file:(Filename.concat tmp_dir filename) content
+    );
+    (* otherwise will get many lookup failure when build the graph_code *)
+    let extra_args = "-nostdlib -nopervasives" in
+    Common.command2 (spf "cd %s; ocamlc -c %s -bin-annot %s"
+                       tmp_dir
+                       extra_args
+                       (* dependency order pbs? assume the given list of files
+                        * is ordered for ocamlc to work, which means generic
+                        * files first and main files at the end.
+                        *)
+                       (files +> List.map fst +> Common.join " "));
+    let skip_list = [] in
+    let g = Graph_code_cmt.build ~verbose:verbose tmp_dir skip_list in
+    let facts = Graph_code_prolog.build tmp_dir g in
+    let facts_pl_file = Filename.concat tmp_dir "facts.pl" in
+    Common.with_open_outfile facts_pl_file (fun (pr_no_nl, _chan) ->
+      let pr s = pr_no_nl (s ^ "\n") in
+      facts +> List.iter (fun x -> pr (Graph_code_prolog.string_of_fact x))
+      );
+    let predicates_file = 
+      Filename.concat Config_pfff.path "h_program-lang/database_code.pl" in
+    if verbose 
+    then Common.cat facts_pl_file +> List.iter pr2;
+    let cmd =
+      spf "swipl -s %s -f %s -t halt --quiet -g \"%s ,fail\""
+        facts_pl_file predicates_file query
+    in
+    let xs = Common.cmd_to_list cmd in
+    xs
+  ) (fun () ->
+    Common.command2 (spf "rm -f %s/*" tmp_dir);
+    Unix.rmdir tmp_dir
+  )
+
 let unittest = 
 "analyze_ml" >::: [
 
@@ -26,6 +71,28 @@ let unittest =
     ()
   );
 
+(*****************************************************************************)
+(* Prolog queries *)
+(*****************************************************************************)
+ "prolog_ml" >::: ([
+   
+   "kind" >:: (fun () ->
+     let files = [
+"pervasives.ml", "
+type 'a ref = { mutable contents : 'a }
+external ref : 'a -> 'a ref = \"%makemutable\"
+";
+"foo.ml", "
+let f x = x
+let g = Pervasives.ref 0
+";] in
+     assert_equal 
+       ["function"]  (prolog_query ~files "kind(('Foo','f'), X), writeln(X)");
+     assert_equal 
+       ["global"]  (prolog_query ~files "kind(('Foo','g'), X), writeln(X)");
+   );
+
+ ])
 
 (*****************************************************************************)
 (* Postlude *)
