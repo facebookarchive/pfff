cd9d0091c2cd3295b20f0d1b965ca371f69747a1
pad <pad@fb.com>
2013-01-08 14:08:03 -0800

    * data/php_stdlib/: split in some files, so some can be skipped in skiplist.txt

--- data/php_stdlib/facebook.php
+++ data/php_stdlib/facebook.php
@@ -1,48 +1,6 @@
 <?php
 
 // ------------------------------------------
-// Flib constants
-// ------------------------------------------
-
-// see also static $builtin_defines = ...
-// in www/flib/_bin/_checkModuleLib.php
-// now in hphp_constants.php
-// define('CLOCK_THREAD_CPUTIME_ID', 1);
-
-// flib/ code is supposed to never use flib_init() so never use
-// any FLIB_CONTEXT_xxx constants, but some of our
-// code sometimes do:
-//   if (ModuleStack::getContext() === FLIB_CONTEXT_TEST) ...
-define('FLIB_CONTEXT_TEST', 1);
-
-// flib/init/http/safety.php also uses those constants to determine which 
-// pages should be 404'ed in the secure tier.
-// mostly copy paste of flib/__flib.php
-define('FLIB_CONTEXT_NOOP', -1);
-define('FLIB_CONTEXT_NULL', 0);
-//define('FLIB_CONTEXT_TEST', 1); done above
-define('FLIB_CONTEXT_SCRIPT', 2);
-define('FLIB_CONTEXT_AJAX', 3);
-define('FLIB_CONTEXT_WEB', 4);
-define('FLIB_CONTEXT_WEB_LITE', 5);
-define('FLIB_CONTEXT_WEB_INTERN', 6);
-define('FLIB_CONTEXT_RESOURCE', 7);
-define('FLIB_CONTEXT_AGENT', 8);
-define('FLIB_CONTEXT_REDIRECT', 9);
-define('FLIB_CONTEXT_LOGGING', 10);
-define('FLIB_CONTEXT_ALITE', 11);
-define('FLIB_CONTEXT_PAGELET', 12);
-define('FLIB_CONTEXT_API', 13);
-define('FLIB_CONTEXT_THRIFT', 14);
-define('FLIB_CONTEXT_DEVSITE', 15);
-define('FLIB_CONTEXT_SECURE_WEB', 16);
-define('FLIB_CONTEXT_CANVAS', 17);
-define('FLIB_CONTEXT_CODESITE', 18);
-define('FLIB_CONTEXT_FBIPHONE_ENDPOINT', 19);
-define('FLIB_CONTEXT_CANVAS_ALITE', 20);
-define('FLIB_CONTEXT_EXTERNAL_DOMAIN', 21);
-
-// ------------------------------------------
 // Misc
 // ------------------------------------------
 
---
+++ data/php_stdlib/hphp_constants_bis.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DEBUG_LOG_AGENT', 8);
+define('DEBUG_LOG_ALL', 65535);
+define('DEBUG_LOG_COOKIE', 128);
+define('DEBUG_LOG_DEFAULT', 111);
+define('DEBUG_LOG_IP', 64);
+define('DEBUG_LOG_NONE', 0);
+define('DEBUG_LOG_POST', 16);
+define('DEBUG_LOG_REFERER', 4);
+define('DEBUG_LOG_TRACE', 1);
+define('DEBUG_LOG_URL', 2);
+define('DEBUG_LOG_USER', 32);
