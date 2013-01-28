97a09667d0aa122c50247ca9d5fed9047dd655c0
pad <pad@fb.com>
2013-01-28 11:05:13 -0800

    * tests/objc/parsing/access.h:

---
+++ tests/objc/parsing/access.h
@@ -0,0 +1,12 @@
+#import <Foundation/NSObject.h>
+
+@interface Access: NSObject {
+@public
+    int publicVar;
+@private
+    int privateVar;
+    int privateVar2;
+@protected
+    int protectedVar;
+}
+@end
