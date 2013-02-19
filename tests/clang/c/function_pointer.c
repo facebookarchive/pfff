e96218f576bde4b2c0ea24feb75d0424c6788a83
Yoann Padioleau <yoann.padioleau@padioleaumbp15.thefacebook.com>
2013-02-19 14:33:17 -0800

    * tests/clang/c/function_pointer.c: support

---
+++ tests/clang/c/function_pointer.c
@@ -0,0 +1,7 @@
+void func_misc1() {
+}
+
+void use_function_pointer() {
+  void (*p)() = &func_misc1;
+}
+
--- tests/clang/c/global.c
+++ tests/clang/c/global.c
@@ -1,13 +1,8 @@
 int global = 1;
 
-void func_misc1() {
-}
-
 void use_global(int param) {
   int local;
   local++;
   param++;
   global++;
-  void (*p)() = &func_misc1;
-  
 }
