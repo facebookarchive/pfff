4760ddd3b633711bb13d22dc1afb5966cc3b1120
pad <pad@fb.com>
2013-04-05 18:23:23 -0700

    * tests/php/scheck/edit_distance.php:

--- tests/php/scheck/edit_distance.php
+++ tests/php/scheck/edit_distance.php
@@ -4,7 +4,9 @@ class XA {
   public $foo;
 
   public function test_use_of_undefined() {
+    //ERROR: 
     echo $this->foa;
+    //ERROR: 
     echo $this->bar;
   }
 }
@@ -12,6 +14,9 @@ class XA {
 
 function test_undeclared_var() {
   $foo = 1;
+  //ERROR: 
   echo $foa;
+  //ERROR: 
   echo $bar;
+  echo $foo;
 }
