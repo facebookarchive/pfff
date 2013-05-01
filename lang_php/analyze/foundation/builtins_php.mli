57d38074a9ba8c11d64460468db7535a4ef09d41
pad <pad@fb.com>
2013-05-01 16:06:24 -0700

    * lang_php/analyze/foundation/builtins_php.ml: update to new json format

--- lang_php/analyze/foundation/builtins_php.mli
+++ lang_php/analyze/foundation/builtins_php.mli
@@ -1,49 +1,10 @@
 (*s: builtins_php.mli *)
 
 val generate_php_stdlib: 
-  src:Common.dirname (* dir with .idl.php files *) -> 
-  phpmanual_dir:Common.dirname (* dir with php manual xml files *) -> 
+  src:Common.dirname (* dir with .idl.json files *) -> 
+  (* phpmanual_dir:Common.dirname (* dir with php manual xml files *) ->  *)
   dest:Common.dirname (* e.g. data/php_stdlib/ *) ->
   unit
-
-(* not used anymore *)
-
-type idl_type = 
-  | Boolean
-  | Byte
-  | Int16
-  | Int32
-  | Int64
-  | Double
-  | String
-  | Int64Vec
-  | StringVec
-  | VariantVec
-  | Int64Map
-  | StringMap
-  | VariantMap
-  | Object
-  | Resource
-  | Variant
-  | Numeric
-  | Primitive
-  | PlusOperand
-  | Sequence
-  | Any
-  | NULL
-  | Void
-
-type idl_param = {
-  p_name: string;
-  p_type: idl_type;
-  p_isref: bool;
-  p_default_val: string option;
-}
-
-type idl_entry = 
-  | Global of string * idl_type
-  | Function of 
-      string * idl_type * idl_param list * bool (* has variable arguments *)
 
 val actions: unit -> Common.cmdline_actions
 (*x: builtins_php.mli *)
