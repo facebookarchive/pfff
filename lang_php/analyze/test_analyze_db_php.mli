4d02feae7e22c3278077c479d1b8ef851876631f
pad <pad@fb.com>
2013-05-30 17:19:31 -0700

    * lang_php/analyze/foundation/unit_analyze_db_php.ml: deleted, to put back

--- lang_php/analyze/test_analyze_db_php.mli
+++
@@ -1,7 +0,0 @@
-(*s: test_analyze_db_php.mli *)
-(* This makes accessible the different test_xxx functions above from 
- * the command line, e.g. '$ pfff_misc -cfg_php foo.php' will call the 
- * test_cfg_php function.
- *)
-val actions: unit -> Common.cmdline_actions
-(*e: test_analyze_db_php.mli *)
