b685aa3017eb1e11f4869eb9c159b3db45f5f554
pad <pad@fb.com>
2013-05-30 17:22:01 -0700

    * main_test.ml: skip prolog unit tests for now

--- main_test.ml
+++ main_test.ml
@@ -60,7 +60,7 @@ let test regexp =
       Unit_typeinfer_php.unittest;
 (*      Unit_analyze_db_php.unittest; *)
 (*      Unit_static_analysis_simple_php.unittest;*)
-      Unit_prolog_php.unittest;
+(* TODO      Unit_prolog_php.unittest; *)
       Unit_checker_php.unittest;
       (* this one needs xdebug to work *)
       Unit_coverage_php.unittest;
