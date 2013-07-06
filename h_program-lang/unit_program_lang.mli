1e9a0b3db28f7fe7301e74a143fe286a485f9dbf
pad <pad@fb.com>
2013-07-05 17:54:17 -0700

    * h_program-lang/unit_program_lang.mli: estet

--- h_program-lang/unit_program_lang.mli
+++ h_program-lang/unit_program_lang.mli
@@ -1,6 +1,6 @@
 
-(* Returns the testsuite for this directory/. To be concatenated by 
+(* Returns the testsuite for this directory. To be concatenated by 
  * the caller (e.g. in pfff/main_test.ml ) with other testsuites and 
- * run via OUnit.run_test_tt 
+ * run via OUnit.run_test_tt().
  *)
 val unittest: OUnit.test
