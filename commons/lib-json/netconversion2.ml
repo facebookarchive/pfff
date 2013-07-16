44aec02ce50b95634dffdba8506285c56b7f882e
pad <pad@fb.com>
2013-07-16 16:46:28 -0700

    * commons/lib-json/netconversion2.ml: use prerr_string

--- commons/lib-json/netconversion2.ml
+++ commons/lib-json/netconversion2.ml
@@ -2,7 +2,7 @@
 let once = ref false
 let ustring_of_uchar x y =
   if not !once then begin
-    print_string "(lib-json)ustring_of_uchar: Todo\n"; flush stdout;
+    prerr_string "(lib-json)ustring_of_uchar: Todo\n"; flush stderr;
     once := true
   end;
   "PBUSTRINGOFCHAR"
