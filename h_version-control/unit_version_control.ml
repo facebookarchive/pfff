9227f92aee73bcaf496443027b453a518d8e8911
pad <pad@fb.com>
2013-07-18 17:08:18 -0700

    pfff: test infrastructure for git API
    Summary:
    Before adding support for mercurial in pfff/h_version-control, it's
    good to put first some basic test infrastructure.
    
    Test Plan: make  test
    
    Reviewers: jiaol, pieter
    
    Reviewed By: pieter
    
    CC: pieter
    
    Differential Revision: https://phabricator.fb.com/D892210
    
    Task ID: 2228702

--- h_version-control/unit_version_control.ml
+++ h_version-control/unit_version_control.ml
@@ -1,12 +1,35 @@
 open Common
 
 open OUnit
-
 module Lib = Lib_vcs
 
 (*****************************************************************************)
 (* Helpers *)
 (*****************************************************************************)
+let with_tmp_directory f =
+  let tmp_dir = Filename.temp_file (spf "vcs-test-%d" (Unix.getpid())) "" in
+  Unix.unlink tmp_dir;
+  (* who cares about race *)
+  Unix.mkdir tmp_dir 0o755;
+  Common.finalize (fun () ->
+    f tmp_dir
+  )
+    (fun () ->
+      (* yep, rm -rf, I'm not scared *)
+      Common.command2 (spf "rm -rf %s/.git" tmp_dir);
+      Common.command2 (spf "rm -rf %s/.hg" tmp_dir);
+      Common.command2 (spf "rm -f %s/*" tmp_dir);
+      Unix.rmdir tmp_dir
+    )
+
+let exec_cmds ~basedir xs =
+  xs +> List.iter (fun cmd ->
+    let exit_status = Sys.command (spf "cd %s; %s" basedir cmd) in
+    match exit_status with
+    | 0 -> ()
+    | _ -> 
+      failwith (spf "command '%s' failed, exit code = %d" cmd exit_status)
+  )
 
 (*****************************************************************************)
 (* Unit tests *)
@@ -14,16 +37,11 @@ module Lib = Lib_vcs
 
 let unittest =
   "version_control" >::: [
-    "parsing file status" >:: (fun () ->
-      assert_equal
-        (Lib.Added, "a/b/foo.php")
-        (Lib.parse_file_status "A\ta/b/foo.php");
 
-      assert_equal
-        (Lib.Renamed (99, "a/b/foo.php"), "a/c/bar.php")
-        (Lib.parse_file_status "R099\ta/b/foo.php\ta/c/bar.php");
+(*****************************************************************************)
+(* Basic *)
+(*****************************************************************************)
 
-    );
     "find root" >:: (fun () ->
       let pfff_home = Config_pfff.path in
       try
@@ -41,5 +59,39 @@ let unittest =
           (fun () -> Git.find_root_from_absolute_path "/");
       with Not_found ->
         assert_failure "fail to find root for pfff_home";
+    );
+
+    "parsing file status" >:: (fun () ->
+      assert_equal
+        (Lib.Added, "a/b/foo.php")
+        (Lib.parse_file_status "A\ta/b/foo.php");
+
+      assert_equal
+        (Lib.Renamed (99, "a/b/foo.php"), "a/c/bar.php")
+        (Lib.parse_file_status "R099\ta/b/foo.php\ta/c/bar.php");
+
+    );
+
+(*****************************************************************************)
+(* Git *)
+(*****************************************************************************)
+
+    "git files in diff involved" >:: (fun () ->
+       with_tmp_directory (fun basedir ->
+         exec_cmds ~basedir [
+           "git init > /dev/null";
+           "echo 'foo' > foo.txt";
+           "echo 'bar' > bar.txt";
+           "git add bar.txt foo.txt";
+           "git commit -m 'first commit' > /dev/null";
+         ];
+         let commit_id = Lib.VersionId "HEAD" in
+         let xs =
+           Git.files_involved_in_diff ~basedir commit_id in
+         assert_equal 
+           ~msg:"it should find all added files in a diff"
+           [Lib.Added, "bar.txt"; Lib.Added, "foo.txt"]
+           (Common.sort xs);
+       );
     );
   ]
