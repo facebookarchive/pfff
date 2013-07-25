4e5048b4c0d8e0ba29048b9d5e2568f8c2e9277e
pad <pad@fb.com>
2013-07-25 16:30:27 -0700

    * travis-ci.sh: more

--- travis-ci.sh
+++ travis-ci.sh
@@ -4,21 +4,27 @@
 # install ocaml
 #------------------------------
 
-# This is too slow, the bandwidth is extremly limited
+# Using apt-get is very very slow but only at the beginning apparently.
+# Once it has downloaded the ocaml package, travis will cache it
+# somewhere and different runs of the sandbox can reuse this cache.
+# But the default version is too old anyway. It does not have
+# compiler-libs which are now needed.
+
 #sudo apt-get update -qq
 #sudo apt-get install -qq ocaml
+#sudo apt-get install -qq ocaml-compiler-libs
 
-# install opam, take less time
-wget http://www.ocamlpro.com/pub/opam_installer.sh
+# install opam, does not take too much time
+#wget http://www.ocamlpro.com/pub/opam_installer.sh
 # can't redirect to >/dev/null because travis
 # put 10min timeout on any program that don't output
 # something on the console
-yes | sh ./opam_installer.sh /usr/local/bin > /dev/null
+#yes | sh ./opam_installer.sh /usr/local/bin > /dev/null
 
 # install packages from opam
 # ??
 
-eval `opam config env`
+#eval `opam config env`
 
 #------------------------------
 # Compile Pfff
