8b6648278d3ce7e36f748e008675ac2634efee30
pad <pad@fb.com>
2013-07-25 16:21:35 -0700

    * travis-ci.sh: try again

--- travis-ci.sh
+++ travis-ci.sh
@@ -6,19 +6,19 @@
 
 # This is too slow, the bandwidth is extremly limited
 #sudo apt-get update -qq
-#sudo apt-get install ocaml-nox
+sudo apt-get install -qq ocaml
 
 # install opam, take less time
-wget http://www.ocamlpro.com/pub/opam_installer.sh
+#wget http://www.ocamlpro.com/pub/opam_installer.sh
 # can't redirect to >/dev/null because travis
 # put 10min timeout on any program that don't output
 # something on the console
-yes | sh ./opam_installer.sh /usr/local/bin
+#yes | sh ./opam_installer.sh /usr/local/bin
 
 # install packages from opam
 # ??
 
-eval `opam config env`
+#eval `opam config env`
 
 #------------------------------
 # Compile Pfff
