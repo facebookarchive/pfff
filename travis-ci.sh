ecada3d4b89f22a5ed412410c2ff7d6fbb688902
pad <pad@fb.com>
2013-07-25 15:02:52 -0700

    * travis-ci.sh:

--- travis-ci.sh
+++ travis-ci.sh
@@ -5,7 +5,7 @@
 #sudo apt-get update -qq
 #sudo apt-get install ocaml-nox
 wget http://www.ocamlpro.com/pub/opam_installer.sh
-echo 'y' | sh ./opam_installer.sh /usr/local/bin
+yes | sh ./opam_installer.sh /usr/local/bin
 
 # install opam
 # TODO
