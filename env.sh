584b7a741e80ab945813398a3ae7bfd238fa49ef
pad <pad@fb.com>
2013-07-25 17:19:53 -0700

    * env.sh: fix

--- env.sh
+++ env.sh
@@ -15,8 +15,9 @@ export LD_LIBRARY_PATH=/home/pad/packages/Linux/lib:$LD_LIBRARY_PATH
 echo setting OPTBIN
 export OPTBIN=.opt
 
-# -bin-annot is for codegraph, needs ocaml 4.00
-export OCAMLCFLAGS_EXTRA=-bin-annot
+# -bin-annot is for codegraph, needs ocaml 4.00, 
+# -absname is for tuareg mode under my mac, also need ocaml 4.00
+export OCAMLCFLAGS_EXTRA=-bin-annot -absname
 
 # for exception stack traces
 echo setting OCAMLRUNPARAM
