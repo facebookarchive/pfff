0773b9f043b98d1dc4db8b23f54cfbd7c0e869f4
pad <pad@fb.com>
2013-07-26 16:48:12 -0700

    * lang_php/analyze/foundation/graph_code_php.mli: clean

--- lang_php/analyze/foundation/graph_code_php.mli
+++ lang_php/analyze/foundation/graph_code_php.mli
@@ -1,4 +1,6 @@
+
 val add_fake_node_when_undefined_entity: bool ref
+
 val build:
   ?verbose:bool -> 
   ?logfile:Common.filename ->
@@ -7,6 +9,4 @@ val build:
   (Common.dirname, Common.filename list) Common.either ->
   Skip_code.skip list ->
   Graph_code.graph
-
-(* internal *)
 
