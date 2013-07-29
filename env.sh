73b72bd139d897722663859ccd07f54b0482dfbf
pieter@fb.com <pieter@fb.com>
2013-07-29 16:30:59 -0700

    add quotes around export= in env.sh
    Summary: This needs quotes for my shell to like it.
    
    Test Plan:
      cd ~/pfff
      source ./env.sh # doesn't complain about -absname being an invalid ident
    
    Reviewers: pad
    
    Reviewed By: pad
    
    Differential Revision: https://phabricator.fb.com/D905321

--- env.sh
+++ env.sh
@@ -1,6 +1,6 @@
 #!!!!You need to source me with "source env.sh" from the _RIGHT_ directory!!!!
 if [ ! -r main.ml ]
-    then echo "There is no main.ml here. 
+    then echo "There is no main.ml here.
 Are you sure you run this script from the source directory ?
 ";
 fi
@@ -15,9 +15,9 @@ export LD_LIBRARY_PATH=/home/pad/packages/Linux/lib:$LD_LIBRARY_PATH
 echo setting OPTBIN
 export OPTBIN=.opt
 
-# -bin-annot is for codegraph, needs ocaml 4.00, 
+# -bin-annot is for codegraph, needs ocaml 4.00,
 # -absname is for tuareg mode under my mac, also need ocaml 4.00
-export OCAMLCFLAGS_EXTRA=-bin-annot -absname
+export OCAMLCFLAGS_EXTRA="-bin-annot -absname"
 
 # for exception stack traces
 echo setting OCAMLRUNPARAM
