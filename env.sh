813e35a1a90eb1c003f37a1a98c696633a9527e6
pad <pad@fb.com>
2013-10-02 11:36:04 -0700

    * env.sh:

--- env.sh
+++ env.sh
@@ -1,28 +1,27 @@
-#!!!!You need to source me with "source env.sh" from the _RIGHT_ directory!!!!
+#!!!!You need to source me with "source env.sh" from the right directory!!!!
 if [ ! -r main.ml ]
     then echo "There is no main.ml here.
-Are you sure you run this script from the source directory ?
+Are you sure you ran this script from the source directory of pfff?
 ";
 fi
 
 # To compile the source, using pad installation.
-echo setting PATH
-export PATH=/home/pad/packages/Linux/bin:$PATH
-echo setting LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/pad/packages/Linux/lib:$LD_LIBRARY_PATH
+echo setting OPAM
+eval `~/packages/Linux/bin/opam config env`
 
 # for faster compiler
 echo setting OPTBIN
 export OPTBIN=.opt
 
-# -bin-annot is for codegraph, needs ocaml 4.00,
-# -absname is for tuareg mode under my mac, also need ocaml 4.00
+# -bin-annot is for codegraph, it needs ocaml 4.00,
+# -absname is for tuareg mode under my mac, it also needs ocaml 4.00
 export OCAMLCFLAGS_EXTRA="-bin-annot -absname"
 
 # for exception stack traces
 echo setting OCAMLRUNPARAM
 export OCAMLRUNPARAM="b"
 
-# To run. To find the config/ files.
+# To run, to find the data/ config files, and to run the tests, 
+# to find the tests/ files.
 echo setting PFFF_HOME
 export PFFF_HOME=`pwd`
