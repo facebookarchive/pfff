048a3490ca06ab8fbed0a525980777c001aef014
pad <pad@fb.com>
2013-10-02 15:54:08 -0700

    * lang_js/parsing/flag_parsing_js.ml: clean verbose_visit

--- lang_js/parsing/flag_parsing_js.ml
+++ lang_js/parsing/flag_parsing_js.ml
@@ -1,7 +1,6 @@
 
 let verbose_lexing = ref false
 let verbose_parsing = ref true
-let verbose_visit = ref true
 
 let debug_lexer   = ref false
 
@@ -16,7 +15,6 @@ let error_recovery = ref false
 let cmdline_flags_verbose () = [
   "-no_verbose_parsing_js", Arg.Clear verbose_parsing , "  ";
   "-no_verbose_lexing_js", Arg.Clear verbose_lexing , "  ";
-  "-no_verbose_visit_js", Arg.Clear verbose_visit , "  ";
 ]
 let cmdline_flags_debugging () = [
   "-debug_lexer_js",        Arg.Set  debug_lexer , " ";
