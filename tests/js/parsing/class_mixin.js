fd07c2b943a98f3c30909095e2cbc6502ab9e080
pad <pad@fb.com>
2013-10-06 16:51:45 -0700

    * tests/js/parsing/class_extends_dot.js: support

--- tests/js/parsing/class_mixin.js
+++ tests/js/parsing/class_mixin.js
@@ -4,3 +4,13 @@ class ObservableTextInputControl extends classWithMixins(TextInputControl, mixin
 class ObservableTextInputControl extends classWithMixins(TextInputControl, mixin(ArbiterMixin)) {
 }
 
+class DPTokenizer extends classWithMixins(Tokenizer, mixin(ArbiterMixin)) {
+}
+
+class AlertManagerSearchQuery
+     extends mixin(ArbiterMixin, FilterQueryMixin,
+                  PageTransitionMixin, SearchTableColumnMixin) {
+}
+
+class TabularLayout extends mixin(new Layout()) {
+}
