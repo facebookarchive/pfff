06dcb644b8744af1d5f5574a34f747d8e636ec3a
pad <pad@fb.com>
2013-10-05 17:56:25 -0700

    * configure: allow 4.00 and 4.01

--- env.sh
+++ env.sh
@@ -6,21 +6,13 @@ Are you sure you ran this script from the source directory of pfff?
 fi
 
 # To compile the source, using pad installation.
-#echo setting OPAM
-#eval `~pad/packages/Linux/bin/opam config env`
+echo setting OPAM
+#eval `~pad/packages/Linux/bin/opam config env` does not work, it expands
+# to /home/pieter/.opam/... when done by another user => inlined here
 export CAML_LD_LIBRARY_PATH=/home/pad/.opam/4.01.0/lib/stublibs
 export OCAML_TOPLEVEL_PATH=/home/pad/.opam/4.01.0/lib/toplevel
 export MANPATH=/home/pad/.opam/4.01.0/man:
 export PATH=/home/pad/.opam/4.01.0/bin:/home/pad/packages/sbin:/home/pad/packages/bin:/home/pad/bin:/usr/kerberos/bin:/opt/local/bin:/usr/local/bin:/bin:/usr/bin:/usr/facebook/ops/scripts:/usr/facebook/scripts:/usr/facebook/scripts:/usr/facebook/scripts/db:/usr/local/sbin:/usr/sbin:/sbin:/mnt/vol/engshare/svnroot/tfb/trunk/www/scripts/bin:/mnt/vol/engshare/admin/scripts/hg:/mnt/vol/engshare/admin/scripts/git:/mnt/vol/engshare/admin/scripts:/home/pad/www/scripts/bin:/home/pad/packages/Linux/bin
-
-
-# for faster compiler
-echo setting OPTBIN
-export OPTBIN=.opt
-
-# -bin-annot is for codegraph, it needs ocaml 4.00,
-# -absname is for tuareg mode under my mac, it also needs ocaml 4.00
-export OCAMLCFLAGS_EXTRA="-bin-annot -absname"
 
 # for exception stack traces
 echo setting OCAMLRUNPARAM
@@ -30,3 +22,7 @@ export OCAMLRUNPARAM="b"
 # to find the tests/ files.
 echo setting PFFF_HOME
 export PFFF_HOME=`pwd`
+
+# for ocamlgtk
+echo setting PKG_CONFIG_PATH
+export PKG_CONFIG_PATH=/usr/X11/lib/pkgconfig
