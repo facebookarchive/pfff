831acf8e1d37e25527f1f881cf56e300e177f011
pad <pad@fb.com>
2013-10-23 12:28:17 -0700

    * main_codegraph.ml: -test_transitive_deps

--- main_codegraph.ml
+++ main_codegraph.ml
@@ -538,6 +538,51 @@ let test_layering graph_file =
   Layer_graph_code.gen_heatmap_layer g htopdown output;
   ()
 
+let test_transitive_deps xs =
+  let pwd = Sys.getcwd () in
+  let file = dep_file_of_dir pwd in
+  let g = Graph_code.load file in
+
+  let hdone = Hashtbl.create 101 in
+  
+  let start_nodes = 
+    xs +> List.map (fun path ->
+      let node =
+        if Sys.is_directory path
+        then path, E.Dir
+        else path, E.File
+      in
+      if not (GC.has_node node g)
+      then failwith (spf "could not find %s" path);
+      node
+    )
+  in
+  let rec dfs depth xs =
+    match xs with
+    | [] -> ()
+    | n::xs ->
+        (if Hashtbl.mem hdone n || depth > 3
+        then ()
+        else begin
+          Hashtbl.add hdone n true;
+          let uses = GC.succ n GC.Use g in
+          dfs (depth + 1) uses;
+          let children = GC.children n g in
+          dfs (depth + 1) children
+        end);
+        dfs depth xs
+      
+  in
+  dfs 0 start_nodes;
+  let files = hdone +> Common.hashset_to_list +> Common.map_filter (fun n ->
+    try 
+      let file = GC.file_of_node n g in
+      Some file
+    with Not_found -> None
+  ) +> Common.hashset_of_list +> Common.hashset_to_list in
+  (*pr2 (spf "%d" (List.length files));*)
+  files +> List.iter pr;
+  ()
 
 (* ---------------------------------------------------------------------- *)
 let extra_actions () = [
@@ -564,6 +609,8 @@ let extra_actions () = [
   Common.mk_action_1_arg test_adhoc_deps;
   "-test_layering", " <graph>",
   Common.mk_action_1_arg test_layering;
+  "-test_transitive_deps", " <dirs or files>",
+  Common.mk_action_n_arg test_transitive_deps;
 (*
   "-test_phylomel", " <geno file>",
   Common.mk_action_1_arg test_phylomel;
