3dc38633aac4cfd6319d965da6c62ea75ee167c3
pad <pad@fb.com>
2013-10-24 17:27:10 -0700

    * main_codegraph.ml: put back GC tuning, big diff on www

--- main_codegraph.ml
+++ main_codegraph.ml
@@ -677,10 +677,8 @@ let options () = [
 let main () = 
   Gc.set {(Gc.get ()) with Gc.stack_limit = 1000 * 1024 * 1024};
   (* see http://www.elehack.net/michael/blog/2010/06/ocaml-memory-tuning *)
-(*
   Gc.set { (Gc.get()) with Gc.minor_heap_size = 2_000_000 };
   Gc.set { (Gc.get()) with Gc.space_overhead = 200 };
-*)
 
   (* Common_extra.set_link(); *)
   let usage_msg = 
