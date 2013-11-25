de0499fa8e139c15797de056bfaaa8edc5a34fb2
pad <pad@fb.com>
2013-11-24 16:48:48 -0800

    * main_spatch.ml: comment

--- main_spatch.ml
+++ main_spatch.ml
@@ -30,6 +30,7 @@ module R = Refactoring_code
  * 
  * related: 
  *  - http://www.jetbrains.com/idea/documentation/ssr.html
+ *  - go has a replace option in its formating tool http://golang.org/cmd/gofmt/
  *)
 
 (*****************************************************************************)
