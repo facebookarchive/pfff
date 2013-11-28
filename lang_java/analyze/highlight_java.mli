4462f40c5694dae8553a1cf395d1c337581e8dcc
pad <pad@fb.com>
2013-11-28 11:20:52 -0800

    * lang_java/analyze/highlight_java.mli: estet

--- lang_java/analyze/highlight_java.mli
+++ lang_java/analyze/highlight_java.mli
@@ -1,7 +1,6 @@
 
 val visit_toplevel :
-  tag_hook:
-    (Parse_info.info -> Highlight_code.category -> unit) ->
+  tag_hook: (Parse_info.info -> Highlight_code.category -> unit) ->
   Highlight_code.highlighter_preferences ->
   (*(Database_php.id * Common.filename * Database_php.database) option -> *)
   Ast_java.program * Parser_java.token list ->
