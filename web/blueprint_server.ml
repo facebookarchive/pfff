58cc14aa0295cb33bb01e9a5f1c4d1109ea62e18
pad <pad@fb.com>
2013-12-11 18:38:15 -0800

    [blueprint] adding edges
    Test Plan:
    http://pad.dev469.prn1.facebook.com:8080/blueprint?class=LoginHandler
    ...
     ],
      "links": [
        [
          "genHandleWrongPasswordString",
          "raiseOnValidExtendedAccountRecoveryPassword"
        ],
        [ "getLoginValidationResult", "$isAppPasswordAllowed" ],
        [ "getLoginValidationResult", "$credentialsChecker" ],
        [ "updateLUCookieAfterLoginSuccess", "$lhProvidedIdentifier" ],
    
    Reviewers: akuhn
    
    CC: bryano
    
    Differential Revision: https://phabricator.fb.com/D1095079

--- web/blueprint_server.ml
+++ web/blueprint_server.ml
@@ -76,11 +76,17 @@ let main_service = Eliom_registration.String.register_service
               "tooltip", J.String "TODO";
           ];
           ));
-        "links", J.Array [
-          J.Array [
-            J.String classname; J.String classname
-          ]
-        ]
+        "links", J.Array (
+          members +> List.map (fun node ->
+            (g +> G.succ node G.Use)
+              +> List.filter (fun node -> Hashtbl.mem hmembers node)
+              +> List.map (fun node2 ->
+                let src = G.shortname_of_node node in
+                let dst = G.shortname_of_node node2 in
+                J.Array [ J.String src; J.String dst]
+              )
+        ) +> List.flatten
+        )
       ]
     in
     let str = Json_out.string_of_json json in
