abe35b8cd5363761e06f7f44712fd966744edca3
pad <pad@fb.com>
2014-01-29 14:08:32 -0800

    * find_source.ml:

--- find_source.ml
+++ find_source.ml
@@ -1,12 +1,21 @@
 open Common
 
+
+let finder lang = 
+  match lang with
+  | "php" | "phpfuzzy" -> 
+    Lib_parsing_php.find_php_files_of_dir_or_files ~verbose:false
+  | "c++" -> 
+    Lib_parsing_cpp.find_source_files_of_dir_or_files
+  | "ml" -> 
+    Lib_parsing_ml.find_source_files_of_dir_or_files
+  | "java" -> 
+    Lib_parsing_java.find_source_files_of_dir_or_files
+  | "js"  -> 
+    Lib_parsing_js.find_source_files_of_dir_or_files ~include_scripts:false
+  | _ -> failwith ("unsupported language: " ^ lang)
+
 let files_of_dir_or_files ~lang ~verbose xs =
   let xs = List.map Common.realpath xs in
-  (match lang with
-  | "php" | "phpfuzzy" -> Lib_parsing_php.find_php_files_of_dir_or_files xs
-  | "c++" -> Lib_parsing_cpp.find_source_files_of_dir_or_files xs
-  | "ml" -> Lib_parsing_ml.find_source_files_of_dir_or_files xs
-  | "java" -> Lib_parsing_java.find_source_files_of_dir_or_files xs
-  | "js"  -> Lib_parsing_js.find_source_files_of_dir_or_files xs
-  | _ -> failwith ("unsupported language: " ^ lang)
-  ) +> Skip_code.filter_files_if_skip_list ~verbose
+  let finder = finder lang in
+  finder xs +> Skip_code.filter_files_if_skip_list ~verbose
