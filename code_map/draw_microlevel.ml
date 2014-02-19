ef51bbcecd597d3e89b7c73ee0bfe8c57ad4d4d2
pad <pad@fb.com>
2014-02-18 17:45:33 -0800

    * code_map/draw_microlevel.ml: estet

--- code_map/draw_microlevel.ml
+++ code_map/draw_microlevel.ml
@@ -204,11 +204,11 @@ let glyphs_of_file ~context ~font_size ~font_size_real file
       let xs = Common2.lines_with_nl_either s in
       xs +> List.iter (function
       | Common2.Left str ->
-        Common.push2 { M. str; font_size = final_font_size; color; categ } acc;
+          Common.push2 { M. str; font_size=final_font_size; color; categ } acc;
       | Common2.Right () ->
-        arr.(!line) <- List.rev !acc;
-        acc := [];
-        incr line;
+          arr.(!line) <- List.rev !acc;
+          acc := [];
+          incr line;
       )
     );
     if !acc <> []
