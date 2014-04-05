5c4ec506a8527f94c964a74ffe2f02c179289292
pad <pad@fb.com>
2014-04-04 17:38:28 -0700

    * lang_cpp/parsing/unit_parsing_cpp.ml: add regression tests for C

--- lang_cpp/parsing/unit_parsing_cpp.ml
+++ lang_cpp/parsing/unit_parsing_cpp.ml
@@ -59,8 +59,20 @@ let unittest =
      )
    );
    
- (* todo: parsing C files (and not C++ files) containing C++ keywords 
- *)
+ (* parsing C files (and not C++ files) possibly containing C++ keywords *)
+   "C regression files" >:: (fun () ->
+     let dir = Filename.concat Config_pfff.path "/tests/c/parsing" in
+     let files = 
+       Common2.glob (spf "%s/*.c" dir)
+       (* @ Common2.glob (spf "%s/*.h" dir) *) in
+     files +> List.iter (fun file ->
+       try
+         let _ast = parse file in
+         ()
+       with Parse_cpp.Parse_error _ ->
+         assert_failure (spf "it should correctly parse %s" file)
+     )
+   );
 
  (*-----------------------------------------------------------------------*)
  (* Misc *)
