7f178fb7055b04b014422ecb5d2c57bad867acb0
pad <pad@fb.com>
2014-04-30 13:59:12 -0700

    * project.el: misc

--- project.el
+++ project.el
@@ -660,7 +660,7 @@
    (join-string 
     (list 
      "-debugger"
-     (case 10
+     (case 0
        (0 "-test")
        (1 "--flib-map /home/pad/www/flib/_bin/.flib.map /home/pad/www/flib/buffy/")
        (2 "--strict /home/pad/www/flib/site/tools/pfff/")
