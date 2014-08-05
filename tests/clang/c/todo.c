62afc56ca3e79ebba7a50536cf9686cc980e4110
pad <pad@fb.com>
2014-08-05 16:47:58 -0700

    * tests/clang/c/todo.c:

---
+++ tests/clang/c/todo.c
@@ -0,0 +1,9 @@
+
+typedef struct {
+  ...
+} Foo;
+
+// and then using fields, I get some "member access to typedef not expanded
+// errors :(
+
+// and when name the struct, get some Dead Type errors in scheck :(
