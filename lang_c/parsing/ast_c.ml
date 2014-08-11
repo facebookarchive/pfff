2d1d83dc8dc8ba7f16df658e366e9ab153a5422c
pad <pad@fb.com>
2014-08-11 13:59:49 -0700

    * lang_c/parsing/ast_c.ml: misc

--- lang_c/parsing/ast_c.ml
+++ lang_c/parsing/ast_c.ml
@@ -1,6 +1,6 @@
 (* Yoann Padioleau
  *
- * Copyright (C) 2012 Facebook
+ * Copyright (C) 2012, 2014 Facebook
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
