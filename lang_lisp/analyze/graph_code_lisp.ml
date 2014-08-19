f0c5cdf1bfae260470cb805587b959203d43c72c
pad <pad@fb.com>
2014-08-18 18:57:12 -0700

    * lang_lisp/analyze/graph_code_lisp.ml: less lookup failures

--- lang_lisp/analyze/graph_code_lisp.ml
+++ lang_lisp/analyze/graph_code_lisp.ml
@@ -45,6 +45,8 @@ type env = {
   current: Graph_code.node;
   readable_file: Common.filename;
 
+  at_toplevel: bool;
+
   (* error reporting *)
   dupes: (Graph_code.node, bool) Hashtbl.t;
 
@@ -75,6 +77,12 @@ let parse file =
 (* Helpers *)
 (*****************************************************************************)
 
+(* dupe: graph_code_c *)
+let find_existing_node env s candidates last_resort =
+  candidates +> Common.find_opt (fun kind ->
+    G.has_node (s, kind) env.g
+  ) ||| last_resort
+
 let _error s tok =
   failwith (spf "%s: %s" (Parse_info.string_of_info tok) s)
 
@@ -84,7 +92,7 @@ let _error s tok =
 
 let add_node_and_edge_if_defs_mode env (s, kind) tok =
   let node = (s, kind) in
-  if env.phase = Defs then
+  (if env.phase = Defs then
     if G.has_node node env.g
     then begin
       env.pr2_and_log (spf "DUPE entity: %s" (G.string_of_node node));
@@ -106,6 +114,8 @@ let add_node_and_edge_if_defs_mode env (s, kind) tok =
       env.g +> G.add_edge (env.current, node) G.Has;
       env.g +> G.add_nodeinfo node nodeinfo;
     end
+  );
+  { env with current = node }
 
 (*****************************************************************************)
 (* Add edge *)
@@ -139,21 +149,43 @@ let rec extract_defs_uses env ast =
     env.g +> G.add_edge ((dir, E.Dir), node) G.Has;
   end;
   let env = { env with current = (env.readable_file, E.File); } in
-  sexps env ast
+  sexps_toplevel env ast
 
 (* ---------------------------------------------------------------------- *)
 (* Toplevels *)
 (* ---------------------------------------------------------------------- *)
-
+and sexps_toplevel env xs =
+  xs +> List.iter (sexp_toplevel env)
 and sexps env xs =
   xs +> List.iter (sexp env)
 
-and sexp env x =
+and sexp_toplevel env x =
+  sexp_bis env x
+and sexp env x = 
+  sexp_bis { env with at_toplevel = false } x
+
+and sexp_bis env x =
   match x with
   | Sexp (_, [Atom (Id ("provide", _)); Special ((Quote, _), 
                                                   (Atom (Id (s, t))))], _) ->
     let _env = add_node_and_edge_if_defs_mode env (s, E.Module) t in
     ()
+
+  | Sexp (_, Atom (Id ("defmacro", _))::Atom (Id (s, t))::rest, _) 
+      when env.at_toplevel ->
+    let env = add_node_and_edge_if_defs_mode env (s, E.Macro) t in
+    sexps env rest
+  (* todo: use Sexp x below because don't handle long symbol like xxx-`S *)
+  | Sexp (_, Atom (Id ("defun", _))::Atom (Id (s, t))::Sexp x::rest, _)
+      when env.at_toplevel ->
+    let env = add_node_and_edge_if_defs_mode env (s, E.Function) t in
+    sexps env (Sexp x::rest)
+(*
+  | Sexp (_, Atom (Id ("defvar", _))::Atom (Id (s, t))::rest, _) ->
+    let env = add_node_and_edge_if_defs_mode env (s, E.Global) t in
+    sexps env rest
+*)
+
 
   | Sexp (_, [Atom (Id ("require", _)); Special ((Quote, _), 
                                                   (Atom (Id (s, t))))], _) ->
@@ -161,6 +193,25 @@ and sexp env x =
       let node = (s, E.Module) in
       add_use_edge env node t
     end
+
+  | Sexp (_, Atom (Id (s, t))::xs, _) ->
+    if env.phase = Uses &&
+      (G.has_node (s, E.Macro) env.g || G.has_node (s, E.Function) env.g)
+    then
+      let kind = find_existing_node env s 
+        [E.Macro; 
+         E.Function;
+        ]
+        E.Function
+      in
+      add_use_edge env (s, kind) t;
+      sexps env xs
+    else
+      (* todo: too many errors for now, do not generate lookup failure for now 
+       * e.g. all builtins are not taken into account
+      *)
+      sexps env xs
+
 
   (* boilerplate *)
   | Sexp (_, xs, _) -> sexps env xs
@@ -191,6 +242,8 @@ let build ?(verbose=true) root files =
     phase = Defs;
     current = G.pb;
     readable_file = "__filled_later__";
+
+    at_toplevel = true;
 
     dupes = Hashtbl.create 101;
 
