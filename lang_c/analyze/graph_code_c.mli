6da5f74046ae2ef11aa892b485b56e19150d4e2c
pad <pad@fb.com>
2014-08-20 12:25:56 -0700

    * lang_c/analyze/graph_code_c.mli: prolog hook!

--- lang_c/analyze/graph_code_c.mli
+++ lang_c/analyze/graph_code_c.mli
@@ -2,3 +2,10 @@
 val build:
   ?verbose:bool -> Common.dirname -> Common.filename list ->
   Graph_code.graph
+
+val hook_use_edge: 
+  (Graph_code_prolog.context -> bool -> 
+  (Graph_code.node * Graph_code.node) -> 
+   Graph_code.graph -> 
+   unit)
+  ref
